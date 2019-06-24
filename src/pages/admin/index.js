1df511e796a8746d577e2eea0c5cf8d8c1795751
Ruihua Sui <rsui2@illinois.edu>
2019-06-24 01:00:35 -0500

    generalize http request functions

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import { api } from '../../util'
 import _ from 'lodash'
-
+// UI
 import { Tab } from 'semantic-ui-react'
 import { SignOutHeader } from '../../components'
 import TermPane from './term'
 import UniPane from './universities'
 import DepartPane from './department'
-
 import './index.css'
-
-const initialTermData = {name: '', startDate: ''};
-const initialUniData = {name: '', domain: ''};
-const initialDepart = {name: '', acronym: '', universityId: ''};
+// Vars
+import { api } from '../../util'
+const { initialTermData, initialUniData, initialDepartData } = api.initialData;
 
 export class AdminPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       activePane: parseInt(localStorage.getItem('activePane')) || 0,
+
       terms: [],
       term: _.clone(initialTermData),
       editTerm: _.clone(initialTermData),
@@ -28,8 +26,8 @@ export class AdminPage extends React.Component {
       editUni: _.clone(initialUniData),
 
       departments: [],
-      depart: _.clone(initialDepart),
-      editDepart: _.clone(initialDepart),
+      depart: _.clone(initialDepartData),
+      editDepart: _.clone(initialDepartData),
     }
   }
   
@@ -40,7 +38,7 @@ export class AdminPage extends React.Component {
   }
 
   componentWillMount() {
-    // localStorage.removeItem('activePane');
+    localStorage.removeItem('activePane');
   }
 
   setActivePane = index => {
@@ -66,17 +64,17 @@ export class AdminPage extends React.Component {
   }
 
   resetState = name => {
-    if (name === 'term' || name === 'editTerm') this.setState({[name]: _.clone(initialTermData)});
-    else if (name === 'uni' || name === 'editUni') this.setState({[name]: _.clone(initialUniData)});
-    else if (name === 'depart' || name === 'editDepart') this.setState({[name]: _.clone(initialDepart)});
+    name = name.toLowerCase();
+    if (name.includes('term')) this.setState({[name]: _.clone(initialTermData)});
+    else if (name.includes('uni')) this.setState({[name]: _.clone(initialUniData)});
+    else if (name.includes('depart')) this.setState({[name]: _.clone(initialDepartData)});
   }
 
   onFormSubmit = name => {
-    const path = name === 'term' ? 'Terms' : 
-                 name === 'uni' ? 'Universities' : 'Departments';
+    const path = api.getApiPath(name);
     api.postData(path, this.state[name])
       .then(responce => {
-        // console.log(responce.data)
+        console.log(responce.data)
         this.resetState(name);
         this.getAll(path)
         this.refresh();
@@ -87,22 +85,12 @@ export class AdminPage extends React.Component {
   }
 
   onUpdate = (name, obj) => {
-    var path, data = _.clone(this.state[name]);
-    data.id = obj.id;
-    if (name === 'editTerm') {
-      path = 'Terms';
-      data.name = data.name || obj.name;
-      data.startDate = data.startDate || obj.startDate;
-    } else if (name === 'editUni') {
-      path = 'Universities';
-      data.name = data.name || obj.name;
-      data.domain = data.domain || obj.domain;
-    } else if (name === 'editDepart') {
-      path = 'Departments';
-      data.name = data.name || obj.name;
-      data.acronym = data.acronym || obj.acronym;
-      data.universityId = data.universityId || obj.universityId;
+    const path = api.getApiPath(name)
+    var data = _.clone(this.state[name]);
+    for (var key in obj) {
+      data[key] = data[key] || obj[key];
     }
+    data.id = obj.id;
     api.updateData(path, data)
       .then(responce => {
         // console.log(responce.data)
@@ -116,8 +104,7 @@ export class AdminPage extends React.Component {
   }
 
   onDelete = (name, obj) => {
-    const path = name === 'term' ? 'Terms' : 
-                 name === 'uni' ? 'Universities' : 'Departments';
+    const path = api.getApiPath(name);
     api.deleteData(path, obj.id)
       .then(responce => {
         // console.log(responce.data)
@@ -129,11 +116,11 @@ export class AdminPage extends React.Component {
       })
   }
 
-  onFormChange = (e, name, target) => {
+  onFormChange = (e, name, key) => {
     const newData = this.state[name];
-    newData[target] = (target === 'universityId') ? e.value :
-                      (target === 'acronym') ? e.target.value.toUpperCase() : 
-                      e.target.value;
+    newData[key] = (key === 'universityId') ? e.value :
+                   (key === 'acronym') ? e.target.value.toUpperCase() : 
+                    e.target.value;
     this.setState({[name]: newData});
   }
 
--- src/util/http.js
+++ src/util/http.js
@@ -1,14 +1,34 @@
 import axios from 'axios';
 import authentication from 'react-azure-adb2c'
 
+const apiMap = {
+  'term': 'Terms',
+  'uni': 'Universities',
+  'depart': 'Departments',
+}
+
+const initialData = {
+  initialTermData: {name: '', startDate: ''},
+  initialUniData: {name: '', domain: ''},
+  initialDepartData: {name: '', acronym: '', universityId: ''},
+}
+
+/**
+ * Set up http
+ */
 const http = axios.create({
   baseURL: 'https://classtranscribe.ncsa.illinois.edu:4443/api/',
   timeout: 10000,
 });
 
-// export function 
 
+/**
+ * Api 
+ */
 export const api = {
+  apiMap: apiMap,
+  initialData: initialData,
+
   getToken: callBack => {
     if (localStorage.getItem('apiToken') === null) {
       const authToken = authentication.getAccessToken();
@@ -27,11 +47,14 @@ export const api = {
       }).then(response => callBack(response.data)); 
     }
   },
-  getData: function (name) {
-    return http.get(`${name}`)
+
+  // http requests
+  getData: function (name, id) {
+    name = id ? `${name}/${id}` : name;
+    return http.get(name);
   },
   postData: function (name, data) {
-    return http.post(`${name}`, data);
+    return http.post(name, data);
   },
   updateData: function (name, data) {
     return http.put(`${name}/${data.id}`, data);
@@ -39,5 +62,13 @@ export const api = {
   deleteData: function (name, id) {
     console.log(`${name}/${id}`)
     return http.delete(`${name}/${id}`)
+  },
+
+  getApiPath: function(str) {
+    str = str.toLowerCase();
+    console.log(str)
+    for (var key in this.apiMap) {
+      if (str.includes(key)) return this.apiMap[key];
+    }
   }
 }
