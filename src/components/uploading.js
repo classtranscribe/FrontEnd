68c8fdf18634523e74b17af42a105ac8035ff026
Ruihua Sui <rsui2@illinois.edu>
2019-07-03 13:42:10 -0500

    css

--- src/components/uploading.js
+++ src/components/uploading.js
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import {useDropzone} from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 import { Button } from 'react-bootstrap'
 
 /**
