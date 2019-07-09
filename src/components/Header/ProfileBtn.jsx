ddeaa6ed2556f80bb53fd804867fb46e354283f9
Ruihua Sui <rsui2@illinois.edu>
2019-07-09 15:52:38 -0500

    solve focus outline

--- src/components/Header/ProfileBtn.jsx
+++ src/components/Header/ProfileBtn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, Dropdown } from 'semantic-ui-react'
+import { Icon, Dropdown, Button } from 'semantic-ui-react'
 import './index.css'
 
 import { util } from '../../util'
@@ -14,29 +14,30 @@ export default function ProfileBtn({user, onSignOut, darkMode}) {
     </span>
   )
   return (
-    <Dropdown trigger={trigger} direction='left' aria-label="Open menu" title="menu">
+    <Dropdown trigger={trigger} direction='left' aria-label="Open menu" title="menu" closeOnBlur={false}>
       <Dropdown.Menu>
         <Dropdown.Item disabled>Signed in as <strong>{user.name}</strong></Dropdown.Item>
         <Dropdown.Divider />
         <Dropdown.Header content='Switch to ...' />
         <Dropdown.Item 
+          as="a"
           icon={{name:'users', color: 'blue',}} 
           text='Student' 
-          onClick={util.toStudentPage}
+          href={util.links.student()}
           title="Switch to student page"
           aria-label="Switch to student page"
         />
         <Dropdown.Item 
           icon={{name:'student', color: 'blue'}} 
           text='Instructor' 
-          onClick={util.toInstructorPage}
+          href={util.links.instructor()}
           title="Switch to instructor page"
           aria-label="Switch to instructor page"
         />
         <Dropdown.Item 
           icon={{name:'cogs', color: 'blue'}} 
           text='Admin' 
-          onClick={util.toAdminPage}
+          href={util.links.admin()}
           title="Switch to administer page"
           aria-label="Switch to administer page"
         />
@@ -52,6 +53,7 @@ export default function ProfileBtn({user, onSignOut, darkMode}) {
         />
         <Dropdown.Divider />
         <Dropdown.Item 
+          as="button"
           icon='sign-out' 
           text='Sign Out' 
           onClick={onSignOut}
