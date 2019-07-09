3f0125c3b8e63417692ea626680bc36ed15cb0e4
Ruihua Sui <rsui2@illinois.edu>
2019-07-09 15:05:03 -0500

    set loading interval

--- src/pages/Instructor/Offering/EditingPages/PlaylistEditing.jsx
+++ src/pages/Instructor/Offering/EditingPages/PlaylistEditing.jsx
@@ -36,7 +36,7 @@ export function PlaylistEditing ({match: {params: {id, type}}, history}) {
    * GET all the needed info based on the playlist Id
    */
   useEffect(()=> {
-    api.contentLoaded()
+    api.contentLoaded(500)
     if (!isNew) {
       // setLoading(true)
       // api.getData(path, id)
