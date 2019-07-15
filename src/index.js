58b672cce0e85186b5203b1998b9be0a27550dc9
Ruihua Sui <rsui2@illinois.edu>
2019-07-15 17:23:47 -0500

    remove index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import authentication from 'react-azure-adb2c'
