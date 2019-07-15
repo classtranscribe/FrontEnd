c8f6898799f91335e1191e92a4cf30f5be3d0d17
Ruihua Sui <rsui2@illinois.edu>
2019-07-15 17:16:45 -0500

    clear unused vars

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import authentication from 'react-azure-adb2c'
 
 import { 
-  LoginPage as Homepage, 
   OfferingViewing, 
   InstructorProfile, 
   InstructorOffering,
