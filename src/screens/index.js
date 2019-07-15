104c815f0c287228831c86d5463b1ba9f9476f9c
Ruihua Sui <rsui2@illinois.edu>
2019-07-14 22:29:06 -0500

    offline old login page

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,8 +1,8 @@
 export { InstructorProfile  } from "./Instructor/Profile"
 export { InstructorOffering } from "./Instructor/Offering"
 export { OfferingViewing    } from "./OfferingViewing"
-// export { LoginPage          } from "./Login"
 export { VideoPage          } from './Watch'
 export { Admin              } from './Admin'
 
+// export { LoginPage        } from "./Login"
 // export { Homepage         } from "./old-homepage";
