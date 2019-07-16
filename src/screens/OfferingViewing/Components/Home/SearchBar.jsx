4d9a36b067b7022987e668d65f8b1e56c860a679
Ruihua Sui <rsui2@illinois.edu>
2019-07-16 14:28:37 -0500

    build responsive search bar

--- src/screens/OfferingViewing/Components/Home/SearchBar.jsx
+++ src/screens/OfferingViewing/Components/Home/SearchBar.jsx
@@ -2,34 +2,38 @@
  * SearchBar, a sub screen of Home page, shows up when user want to search a course
  */
 
-import React from 'react'
-import { Icon, List } from 'semantic-ui-react';
+import React, { createRef } from 'react'
+import { Icon, List, Sticky } from 'semantic-ui-react';
 import { search } from '../../../../util';
 import { Link } from 'react-router-dom'
 
 export default function SearchBar({onSearching, onInput, state, setCurrentOffering}) {
   if (!state.onSearching) return null
+  const ref = createRef()
   const { offerings, searchValue } = state
   const results = search.getResult(offerings, searchValue)
+
   return (
-    <div className="search-bar" id="search-bar">
+    <div className="search-bar" id="search-bar" ref={ref}>
       <div className="d-flex justify-content-end w-100" onClick={onSearching}>
         <Link className="del-icon">
           <Icon name="chevron left" /> Back to Courses
         </Link>
       </div>
 
-      <div className="sb-input">
-        <div className="ui icon input">
-          <input autoFocus
-            type="text" className="prompt" id="search"
-            value={searchValue}
-            onChange={onInput}
-            placeholder="Search for Courses ..."
-          />
-          <i aria-hidden="true" class="search icon"></i>
+      <Sticky context={ref}>
+        <div className="sb-input">
+          <div className="ui icon input">
+            <input autoFocus
+              type="text" className="prompt" id="search"
+              value={searchValue}
+              onChange={onInput}
+              placeholder="Search for Courses ..."
+            />
+            <i aria-hidden="true" class="search icon"></i>
+          </div>
         </div>
-      </div>
+      </Sticky>
       
       <div className="result">
         <List divided relaxed>
