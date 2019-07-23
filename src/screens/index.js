594e1b63def5afb42bb15acbd580c3db0f57a2a8
Ruihua Sui <rsui2@illinois.edu>
2019-07-23 00:59:45 -0500

    clear comments

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,5 @@
 export { InstructorProfile  } from "./Instructor/Profile"
 export { InstructorOffering } from "./Instructor/Offering"
 export { OfferingViewing    } from "./OfferingViewing"
-// export { VideoPage          } from './WatchOld'
 export { Admin              } from './Admin'
 export { Watch              } from './Watch'
-
-// export { LoginPage        } from "./Login"
-// export { Homepage         } from "./old-homepage";
