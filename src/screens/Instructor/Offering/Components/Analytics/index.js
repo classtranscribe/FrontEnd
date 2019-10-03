b435e40b916741b6752d750645fb1ba9a9bbfe8b
Ruihua Sui <rsui2@illinois.edu>
2019-10-03 18:26:55 -0500

    remove in progress tabs

--- src/screens/Instructor/Offering/Components/Analytics/index.js
+++ src/screens/Instructor/Offering/Components/Analytics/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import {api} from 'utils';
-import { BarsChart, Chart } from "./charts";
 import {AnalyticTable} from './table'
 import VideoViews from './VideoViews'
 import { Tab } from 'semantic-ui-react'
 import './index.css';
-// import { Button } from 'semantic-ui-react';
+
 export class Analytics extends React.Component {
   constructor(props) {
     super(props)
@@ -31,8 +29,8 @@ function MyTabs ({offeringId, playlists}){
   const panes = [
 
     { menuItem: 'Performance', render: () => <AnalyticTable offeringId={offeringId}/> },
-    { menuItem: 'Charts', render: () => <VideoViews offeringId={offeringId} playlists={playlists} />},
-    { menuItem: 'To be developed', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    // { menuItem: 'Charts', render: () => <VideoViews offeringId={offeringId} playlists={playlists} />},
+    // { menuItem: 'To be developed', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
   ]
   return (
      <Tab panes={panes} />
