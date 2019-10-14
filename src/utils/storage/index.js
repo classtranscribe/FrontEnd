0c33e422ae3b6e64e4a2182feb81e3c44b6df41e
Ruihua Sui <rsui2@illinois.edu>
2019-10-13 21:38:07 -0500

    prevent store data on mobile

--- src/utils/storage/index.js
+++ src/utils/storage/index.js
@@ -1,4 +1,4 @@
-import { isMobile } from 'react-device-detect'
+import { isBrowser } from 'react-device-detect'
 import { api } from '../HTTP'
 
 export const storage = {
@@ -23,15 +23,15 @@ export const storage = {
   ttempStoredOfferingsKey: 'temp-stored-offerings',
 
   storeOfferings: function(offerings) {
-    if (!isMobile) localStorage.setItem(this.tempStoredOfferingsKey, JSON.stringify(offerings))
+    if (isBrowser) localStorage.setItem(this.tempStoredOfferingsKey, JSON.stringify(offerings))
   },
   getStoredOfferings: function() {
     // this.removeStoredOfferings()
-    if (isMobile) return null
+    if (!isBrowser) return null
     const storedOfferings_str = localStorage.getItem(this.tempStoredOfferingsKey)
     return storedOfferings_str ? JSON.parse(storedOfferings_str) : null
   },
   removeStoredOfferings: function() {
-    if (!isMobile) localStorage.removeItem(this.tempStoredOfferingsKey)
+    if (isBrowser) localStorage.removeItem(this.tempStoredOfferingsKey)
   },
 }
