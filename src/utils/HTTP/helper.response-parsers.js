269272d30546943d0e38d9e81a2176a98999dc43
Ruihua Sui <rsui2@illinois.edu>
2019-10-16 03:35:10 -0500

    fix css && fix bugs

--- src/utils/HTTP/helper.response-parsers.js
+++ src/utils/HTTP/helper.response-parsers.js
@@ -78,6 +78,7 @@ export const responseParsers = {
         universityId,
         termName: term.name,
         fullNumber: this.getFullNumber(courses),
+        isTestCourse: _.findIndex(courses, { courseNumber: '000' }) >= 0 && !user.isAdmin(),
       })
     }
     // console.log('parsedOfferings', parsedOfferings)
