dcf92e08c976ef1c7f7008e4250ff5bdcaa80b12
Ruihua Sui <rsui2@illinois.edu>
2019-12-04 22:25:44 -0600

    remove unused pkgs & add exp check for auth

--- src/utils/HTTP/helper.response-parsers.js
+++ src/utils/HTTP/helper.response-parsers.js
@@ -24,7 +24,7 @@ export const responseParsers = {
       eventType, 
       mediaId, 
       offeringId,
-      userId: api.userId(),
+      userId: user.userId(),
       json: {
         ...json, 
         device: { deviceType, osVersion, osName, fullBrowserVersion, browserName }
@@ -116,7 +116,7 @@ export const responseParsers = {
       transcriptions: [],
       isUnavailable: false,
     }
-    console.log(media)
+    // console.log(media)
     if (!media) return re
     const { id, playlistId, jsonMetadata, sourceType, video, transcriptions } = media
     if (!id || !jsonMetadata) return re
