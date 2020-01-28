af5f9b7620611392a195cc484e5740de013e9d87
Shawn Sui <ruihua.sui@gmail.com>
2020-01-27 22:32:11 -0600

    remove testing user role

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 export const STUDENT = 'u-stu'
 export const INSTRUCTOR = 'u-inst'
 export const ADMIN = 'u-admin'
-export const DEFAULT_ROLE = INSTRUCTOR // used for testing
+export const DEFAULT_ROLE = STUDENT // used for testing
 
 
 export const isDeveloping = window.location.origin === 'http://localhost:3000'
