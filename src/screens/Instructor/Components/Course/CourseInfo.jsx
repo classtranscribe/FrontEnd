b4bfd9b68c9f95a5e24fa9e060ae6a7f31f9bce6
Shawn Sui <ruihua.sui@gmail.com>
2020-01-30 22:24:34 -0600

    change the position of analytics button

--- src/screens/Instructor/Components/Course/CourseInfo.jsx
+++ src/screens/Instructor/Components/Course/CourseInfo.jsx
@@ -24,39 +24,32 @@ export default function CourseInfo({
         </div>
 
         <div className="ip-c-btns-con ct-btn-group">
-          <Button uppercase
-            text="Edit Course"
-            color="transparent teal"
-            onClick={handleEdit}
-          />
-          <Button round
-            icon="delete"
-            color="transparent"
-            popup="Delete Course"
-            onClick={handleDelete}
-          />
+          <Button.Group>
+            {
+              offering.logEventsFlag 
+              &&
+              <Button uppercase
+                text="Analytics"
+                icon="bar_chart"
+                color="primary teal"
+                onClick={viewAnalytics}
+              />
+            }
+            <Button uppercase
+              text="Settings"
+              icon="settings"
+              //color="transparent black"
+              onClick={handleEdit}
+            />
+            <Button round
+              icon="delete"
+              color="transparent"
+              popup="Delete Course"
+              onClick={handleDelete}
+            />
+          </Button.Group>
         </div>
       </div>
-
-      {
-        offering.logEventsFlag 
-        &&
-        <div className="w-100 mb-3">
-          <button 
-            className="plain-btn ip-sb-off-item ip-c-pl-item" 
-            onClick={viewAnalytics}
-          >
-            <div tabIndex="-1" className="ip-sb-off-item-con ip-c-pl-item-con">
-              <span className="ct-d-r-center-v ip-sb-off-text ip-c-pl-name ip-sb-off-num">
-                <i className="material-icons" aria-hidden="true">bar_chart</i> COURSE ANALYSIS
-              </span>
-              <span className="ip-c-pl-r-icon" data-small>
-                <i className="material-icons">chevron_right</i>
-              </span>
-            </div>
-          </button>
-        </div>
-      }
     </>
   )
 }
