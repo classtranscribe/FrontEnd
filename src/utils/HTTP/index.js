63748a63ec6a4cb3fd62c02062c594de66c44a01
Chirantan Mahipal <mahipal2@classtranscribe.ncsa.illinois.edu>
2020-02-22 15:37:43 -0600

    Changing route name

--- src/utils/HTTP/index.js
+++ src/utils/HTTP/index.js
@@ -67,7 +67,7 @@ export const api = {
   },
 
   testAccountSignIn: function(emailId, password) {
-    return http.post(this.baseUrl() + '/api/Account/TestSignIn', { emailId, password })
+    return http.post(this.baseUrl() + '/api/Account/LoginAs', { emailId, password })
   },
 
   withAuth: function (configs) {
