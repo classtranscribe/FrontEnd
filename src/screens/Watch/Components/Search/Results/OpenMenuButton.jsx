742219cb87a91d6cd88406d1ffeaa7e1169bb319
Shawn Sui <ruihua.sui@gmail.com>
2020-03-16 23:05:01 -0500

    Split code of result list

---
+++ src/screens/Watch/Components/Search/Results/OpenMenuButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { menuControl } from '../../../Utils'
+
+export default function OpenMenuButton({
+  show,
+  menu,
+  name,
+}) {
+  return show ? (
+    <div className="w-100 d-flex px-3">
+      <button
+        className="plain-btn watch-search-btn page-btn"
+        aria-label={`See all ${name}`}
+        aria-haspopup="true"
+        onClick={() => menuControl.open(menu)}
+      >
+        <span className="py-2 px-4 my-1 fsize-1-3" tabIndex="-1">See all {name}</span>
+      </button>
+    </div>
+  ) : null
+}
---
+++ src/screens/Watch/Components/Search/Results/PageControlButtons.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function PageControlButtons({
+  isTop=false,
+  page,
+  totalPage,
+  nextPage,
+  prevPage,
+  value,
+  resultsNum,
+}) {
+
+  return (
+    <div className="w-100 d-flex align-content-center justify-content-between search-result-bar">
+      <div className="search-result-term">
+        {
+          isTop 
+          ? 
+          `${resultsNum === 100 ? 'Top 100' : resultsNum} result${resultsNum > 1 ? 's' : '' } for '${value}'` 
+          : 
+          " "
+        }
+      </div>
+      {
+        totalPage > 1 
+        &&
+        <div className="position-relative d-flex align-content-center justify-content-center">
+          <div className="p-1 mx-1">Page {page}/{totalPage}</div>
+          <div>
+            <button
+              className="plain-btn watch-search-btn page-btn"
+              disabled={page === 1}
+              onClick={prevPage}
+            >
+              <span tabIndex="-1"><i className="material-icons">chevron_left</i></span>
+            </button>
+            <button
+              className="plain-btn watch-search-btn page-btn"
+              disabled={page === totalPage}
+              onClick={nextPage}
+            >
+              <span tabIndex="-1"><i className="material-icons">chevron_right</i></span>
+            </button>
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
