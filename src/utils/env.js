ce584cb1337c990a68ac4f5e2848a99184be64aa
Shawn Sui <ruihua.sui@gmail.com>
2020-04-03 00:11:09 -0500

    Modified ReactEnv class

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,36 +1,55 @@
-import { isDeveloping } from "./constants"
+const reactEnv = window.env
 
+/**
+ * Throw the error on missing a required environment variable
+ * @param {String} envName 
+ */
+const missingRequiredEnv = envName => {
+  throw Error('Missing required environment variable ' + envName)
+}
+
+/**
+ * Class used to handle using environment variables
+ */
 class ReactEnv {
-  constructor() {
-    const reactEnv = window.env
-    // console.log('reactEnv', reactEnv)
-    this._baseURL = reactEnv.REACT_APP_API_BASE_URL
-    this._testingBaseURL = reactEnv.REACT_APP_TESTING_BASE_URL
-    this._auth0Domain = reactEnv.AUTH0_DOMAIN
-    this._auth0ClientID = reactEnv.AUTH0_CLIENT_ID
-    this._dev = reactEnv.TEST_SIGN_IN
-    this._frontendCommitEndpoint = reactEnv.REACT_APP_FRONTEND_COMMIT_ENDPOINT
-  }
-
-  get baseURL() {
-    return this.dev ? 
-           this._testingBaseURL : this._baseURL
-  }
-
   get auth0Domain() {
-    return this._auth0Domain
+    if (!reactEnv.AUTH0_DOMAIN) missingRequiredEnv('AUTH0_DOMAIN')
+    return reactEnv.AUTH0_DOMAIN
   }
 
   get auth0ClientID() {
-    return this._auth0ClientID
+    if (!reactEnv.AUTH0_CLIENT_ID) missingRequiredEnv('AUTH0_CLIENT_ID')
+    return reactEnv.AUTH0_CLIENT_ID
   }
 
   get dev() {
-    return this._dev === 'true'
+    return reactEnv.TEST_SIGN_IN === 'true'
   }
 
   get frontendCommitEndpoint() {
-    return this._frontendCommitEndpoint
+    if (!reactEnv.REACT_APP_FRONTEND_COMMIT_ENDPOINT) missingRequiredEnv('REACT_APP_FRONTEND_COMMIT_ENDPOINT')
+    return reactEnv.REACT_APP_FRONTEND_COMMIT_ENDPOINT
+  }
+
+  get azureInstrumentationKey() {
+    if (!reactEnv.REACT_APP_AZURE_INSTRUMENTATION_KEY) missingRequiredEnv('REACT_APP_AZURE_INSTRUMENTATION_KEY')
+    return reactEnv.REACT_APP_AZURE_INSTRUMENTATION_KEY
+  }
+
+  get productionServer() {
+    if (!reactEnv.REACT_APP_API_BASE_URL) missingRequiredEnv('REACT_APP_API_BASE_URL')
+    return reactEnv.REACT_APP_API_BASE_URL
+  }
+
+  get devServer() {
+    if (!reactEnv.REACT_APP_TESTING_BASE_URL) missingRequiredEnv('REACT_APP_TESTING_BASE_URL')
+    return reactEnv.REACT_APP_TESTING_BASE_URL
+  }
+
+  get baseURL() {
+    return this.dev 
+          ? this.devServer
+          : this.productionServer
   }
 }
 
