57136ae7d84a61b87dab57bfd99627c7ae218914
Shawn Sui <ruihua.sui@gmail.com>
2020-04-05 19:32:26 -0500

    update error handler

--- src/utils/env.js
+++ src/utils/env.js
@@ -8,10 +8,21 @@ const missingRequiredEnv = envName => {
   throw Error('Missing required environment variable ' + envName)
 }
 
+const usingWindowOrigin = envName => {
+  console.warn('Missing environment variable ' + envName + '. Start using window.location.origin.')
+}
+
 /**
  * Class used to handle using environment variables
  */
 class ReactEnv {
+  constructor() {
+    if (!reactEnv.AUTH0_DOMAIN) missingRequiredEnv('AUTH0_DOMAIN')
+    if (!reactEnv.AUTH0_CLIENT_ID) missingRequiredEnv('AUTH0_CLIENT_ID')
+    if (!reactEnv.REACT_APP_FRONTEND_COMMIT_ENDPOINT) missingRequiredEnv('REACT_APP_FRONTEND_COMMIT_ENDPOINT')
+    if (!reactEnv.APPLICATION_INSIGHTS_KEY) missingRequiredEnv('APPLICATION_INSIGHTS_KEY')
+  }
+
   get auth0Domain() {
     if (!reactEnv.AUTH0_DOMAIN) missingRequiredEnv('AUTH0_DOMAIN')
     return reactEnv.AUTH0_DOMAIN
@@ -37,13 +48,13 @@ class ReactEnv {
   }
 
   get productionServer() {
-    if (!reactEnv.REACT_APP_API_BASE_URL) missingRequiredEnv('REACT_APP_API_BASE_URL')
-    return reactEnv.REACT_APP_API_BASE_URL
+    if (!reactEnv.REACT_APP_API_BASE_URL) usingWindowOrigin('REACT_APP_API_BASE_URL')
+    return reactEnv.REACT_APP_API_BASE_URL || window.location.origin
   }
 
   get devServer() {
-    if (!reactEnv.REACT_APP_TESTING_BASE_URL) missingRequiredEnv('REACT_APP_TESTING_BASE_URL')
-    return reactEnv.REACT_APP_TESTING_BASE_URL
+    if (!reactEnv.REACT_APP_TESTING_BASE_URL) usingWindowOrigin('REACT_APP_TESTING_BASE_URL')
+    return reactEnv.REACT_APP_TESTING_BASE_URL || window.location.origin
   }
 
   get baseURL() {
