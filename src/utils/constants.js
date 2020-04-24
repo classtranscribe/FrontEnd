4736f201094ef6183751af8701ad57c968adb387
Shawn Sui <ruihua.sui@gmail.com>
2020-04-23 22:19:46 -0500

    set isDeveloping

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,13 +14,12 @@ export const PAGE_HOME = 'homep'
 export const PAGE_MEDIA_SETTING = 'msp'
 export const PAGE_WATCH = 'watch'
 
-export const isDeveloping = false 
-// Boolean(
-//   window.location.hostname === 'localhost' ||
-//     // [::1] is the IPv6 localhost address.
-//     window.location.hostname === '[::1]' ||
-//     // 127.0.0.1/8 is considered localhost for IPv4.
-//     window.location.hostname.match(
-//       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-//     )
-// )
+export const isDeveloping = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+)
