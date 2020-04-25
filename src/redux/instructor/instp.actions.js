92402e19f8447c96b176d3c559d00abeb605ccd0
Shawn Sui <ruihua.sui@gmail.com>
2020-04-24 23:28:01 -0500

    refactor codes in  /redux

--- src/redux/instructor/instp.actions.js
+++ src/redux/instructor/instp.actions.js
@@ -18,25 +18,26 @@ import {
   SET_CONFIRMATION,
   SET_ORDERING,
 } from './instp.action.types'
+import { createAction } from '../redux-creators'
 
 // Sidebar
-export const setSidebar             = value => ({ type: SET_SIDEBAR, value })
-export const setOfferings           = value => ({ type: SET_OFFERINGS, value })
-export const setDeparts             = value => ({ type: SET_DEPARTS, value })
-export const setTerms               = value => ({ type: SET_TERMS, value })
+export const setSidebar             = createAction(SET_SIDEBAR)
+export const setOfferings           = createAction(SET_OFFERINGS)
+export const setDeparts             = createAction(SET_DEPARTS)
+export const setTerms               = createAction(SET_TERMS)
 
 // Course
-export const setOffering            = value => ({ type: SET_OFFERING, value })
-export const setPlaylists           = value => ({ type: SET_PLAYLISTS, value })
-export const setIsEditingOffering   = value => ({ type: SET_IS_EDITING_OFFERING, value })
-export const setIsViewingAnalytics  = value => ({ type: SET_IS_VIEWING_ANALYTICS, value })
+export const setOffering            = createAction(SET_OFFERING)
+export const setPlaylists           = createAction(SET_PLAYLISTS)
+export const setIsEditingOffering   = createAction(SET_IS_EDITING_OFFERING)
+export const setIsViewingAnalytics  = createAction(SET_IS_VIEWING_ANALYTICS)
 
 // Playlist
-export const setPlaylist            = value => ({ type: SET_PLAYLIST, value })
-export const setIsSelectingVideos   = value => ({ type: SET_IS_SELECTING_VIDEOS, value })
-export const setSelectedVideos      = value => ({ type: SET_SELECTED_VIDEOS, value })
+export const setPlaylist            = createAction(SET_PLAYLIST)
+export const setIsSelectingVideos   = createAction(SET_IS_SELECTING_VIDEOS)
+export const setSelectedVideos      = createAction(SET_SELECTED_VIDEOS)
 
 // Others
-export const setLoading             = value => ({ type: SET_LOADING, value })
-export const setConfirmation        = value => ({ type: SET_CONFIRMATION, value })
-export const setOrdering            = value => ({ type: SET_ORDERING, value })
+export const setLoading             = createAction(SET_LOADING)
+export const setConfirmation        = createAction(SET_CONFIRMATION)
+export const setOrdering            = createAction(SET_ORDERING)
