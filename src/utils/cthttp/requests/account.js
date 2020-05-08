28b7d65df6d28dda61b4243d4ffd073eb3207ed7
Shawn Sui <ruihua.sui@gmail.com>
2020-05-08 00:09:36 -0500

    apply new signin API

--- src/utils/cthttp/requests/account.js
+++ src/utils/cthttp/requests/account.js
@@ -5,16 +5,17 @@ import { cthttp } from './request'
 // SignIn
 // ------------------------------------------------------------
 
-export function accountSignIn(auth0Token) {
-    return cthttp.post('Account/SignIn', { auth0Token })
+export function accountSignIn(token, authMethod) {
+    console.log({ token, authMethod });
+    return cthttp.post('Account/SignIn', { token, authMethod });
 }
 
-export function testAccountSignIn() {
-    return cthttp.get('Account/TestSignIn')
+export function testSignIn() {
+    return cthttp.get('Account/TestSignIn');
 }
 
 export function loginAsAccountSignIn(emailId) {
-    return cthttp.post('Account/LoginAs', { emailId })
+    return cthttp.post('Account/LoginAs', { emailId });
 }
 
 // ------------------------------------------------------------
@@ -22,9 +23,9 @@ export function loginAsAccountSignIn(emailId) {
 // ------------------------------------------------------------
 
 export function getUserMetaData() {
-    return cthttp.get('Account/GetUserMetadata/GetUserMetadata')
+    return cthttp.get('Account/GetUserMetadata/GetUserMetadata');
 }
 
 export function postUserMetaData(metadata={}, onboard={}) {
-    return cthttp.post('Account/PostUserMetadata/PostUserMetadata', { ...metadata, onboard })
+    return cthttp.post('Account/PostUserMetadata/PostUserMetadata', { ...metadata, onboard });
 }
