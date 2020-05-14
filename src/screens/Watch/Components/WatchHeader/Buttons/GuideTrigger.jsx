02e77ee6a6d32bb49b9d86cb4a84a6b2ba46b4e4
Shawn Sui <ruihua.sui@gmail.com>
2020-05-14 16:01:54 -0500

    modify watch guide styles

--- src/screens/Watch/Components/WatchHeader/Buttons/GuideTrigger.jsx
+++ src/screens/Watch/Components/WatchHeader/Buttons/GuideTrigger.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import WatchCtrlButton from '../../WatchCtrlButton'
 import { 
-  MENU_HIDE, MENU_DOWNLOAD, 
-  connectWithRedux,
-  menuControl,
+  videoControl,
   generateWatchUserGuide
 } from '../../../Utils'
 
-import { IconButton } from '@material-ui/core'
-import { Icon } from 'semantic-ui-react'
 
-
-function GuideTrigger({handleClick}) {
+function GuideTrigger() {
 
   const handleGuideTrigger = () => {
+    videoControl.pause()
     let watchUserGuide = generateWatchUserGuide()
     watchUserGuide.start()
   }
@@ -23,31 +19,18 @@ function GuideTrigger({handleClick}) {
     <WatchCtrlButton 
       onClick={handleGuideTrigger}
       position="top"
-      label="Guide"
-      id={'GuideButton'}
+      label="Help"
+      id="help-guide-btn"
       ariaTags={{
-        'aria-label': `Guide`,
-        //'aria-keyshortcuts': 'Shift+D',
-        // 'aria-controls': 'watch-download-menu',
-        // 'aria-expanded' : menu === MENU_DOWNLOAD ? 'false' : 'true',
+        'aria-label': `Guide`
       }}
     >
       <span className="watch-btn-content watch-header-btn-content" tabIndex="-1">
-        <i className="material-icons">not_listed_location</i>         
+        <i className="material-icons">help_outline</i>         
       </span>
     </WatchCtrlButton>
   )
 }
 
 export default GuideTrigger
- 
-
-{/* <div className="profile-menu">
-  <IconButton 
-      onClick={handleClick}
-      className="trigger"
-  >
-      <Icon name='question circle' circular/>
-  </IconButton>
-</div> */}
     
