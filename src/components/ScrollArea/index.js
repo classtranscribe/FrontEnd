a183ad607f0ee0025ad4ac1d2b24bbab27656f27
Shawn Sui <ruihua.sui@gmail.com>
2020-05-28 00:38:10 -0500

    Add disable option for ScrollArea

--- src/components/ScrollArea/index.js
+++ src/components/ScrollArea/index.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Button } from 'pico-ui';
 import './index.scss';
@@ -16,19 +17,25 @@ function addScrollEventListener(scrollEl, offsetTop, setIsTop) {
   });
 }
 
-export function ScrollArea({
-  id = null,
-  className = null,
-  scrollClassName = null,
-  offsetTop = 0,
-  scrollToTopButton = 'hide', // 'hide', 'top left', 'top right', 'bottom left', 'bottom right'
-  scrollToTopButtonStyle = {},
-  scrollBar = true,
-  dark = false,
-  smoothScroll = false,
-  children = null,
-  ...otherProps
-}) {
+/**
+ * A controlled scrolling wrapper
+ */
+export function ScrollArea(props) {
+  let {
+    id,
+    className,
+    scrollClassName,
+    offsetTop = 0,
+    scrollToTopButton = 'hide', // 'hide', 'top left', 'top right', 'bottom left', 'bottom right'
+    scrollToTopButtonStyle = {},
+    scrollBar = true,
+    dark = false,
+    smoothScroll = false,
+    children,
+    disabled = false,
+    ...otherProps
+  } = props;
+
   const scrollRef = useRef();
   const [isTop, setIsTop] = useState(true);
 
@@ -37,7 +44,7 @@ export function ScrollArea({
       scrollRef.current.style.scrollBehavior = 'smooth';
     }
 
-    if (scrollToTopButton !== 'hide') {
+    if (scrollToTopButton !== 'hide' && !disabled) {
       addScrollEventListener(scrollRef.current, offsetTop, setIsTop);
     }
   }, []);
@@ -50,20 +57,70 @@ export function ScrollArea({
     'is-top': isTop,
   });
 
+  const scrollProps = { 
+    id,
+    ref: scrollRef,
+    className: scrollClasses
+  };
+
+  if (scrollBar && !disabled) {
+    scrollProps['data-scroll'] = 'true';
+  }
+
   return (
     <div className={classes}>
-      <div style={scrollToTopButtonStyle} className={scrollTopBtnClasses}>
-        <Button round icon="arrow_upward" color={dark ? 'teal' : 'black'} onClick={scrollToTop} />
-      </div>
-      <div
-        id={id}
-        ref={scrollRef}
-        data-scroll={scrollBar}
-        className={scrollClasses}
-        {...otherProps}
-      >
+      {
+        !disabled
+        &&
+        <div style={scrollToTopButtonStyle} className={scrollTopBtnClasses}>
+          <Button round icon="arrow_upward" color={dark ? 'teal' : 'black'} onClick={scrollToTop} />
+        </div>
+      }
+
+      <div {...scrollProps} {...otherProps}>
         {children}
       </div>
     </div>
   );
+}
+
+ScrollArea.propTypes = {
+  /** Id of the element */
+  id: PropTypes.string,
+
+  /** Classes of the container */
+  className: PropTypes.string,
+
+  /** Classes of scrollable area */
+  scrollClassName: PropTypes.string,
+
+  /** Offset number to determine is top or not */
+  offsetTop: PropTypes.number,
+
+  /** Position of the scroll to top button */
+  scrollToTopButton: PropTypes.oneOf([
+    'hide', 
+    'top left', 
+    'top right', 
+    'bottom left', 
+    'bottom right'
+  ]),
+
+  /** Styles for the scroll to top button */
+  scrollToTopButtonStyle: PropTypes.any,
+
+  /** Display the scroll bar */
+  scrollBar: PropTypes.bool,
+
+  /** ScrollArea supports darkMode */
+  dark: PropTypes.bool,
+
+  /** ScrollArea supports smooth scrolling */
+  smoothScroll: PropTypes.bool,
+
+  /** Primary element */
+  children: PropTypes.node,
+
+  /** Disable scrolling */
+  disabled: PropTypes.bool,
 }
