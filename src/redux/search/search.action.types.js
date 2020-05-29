26672468641262d157f545d9078203eaa06762be
Shawn Sui <ruihua.sui@gmail.com>
2020-05-28 23:02:15 -0500

    Setup search page redux store

---
+++ src/redux/search/search.action.types.js
@@ -0,0 +1,3 @@
+export const SET_OFFERINGS = 'search-set-offerings';
+export const SET_SEARCH_VAL = 'search-set-search-value';
+export const SET_SEARCH_RES = 'search-set-search-result';
---
+++ src/redux/search/search.actions.js
@@ -0,0 +1,10 @@
+import {
+  SET_OFFERINGS,
+  SET_SEARCH_VAL,
+  SET_SEARCH_RES,
+} from './search.action.types';
+import { createAction } from '../redux-creators';
+
+export const setOfferings = createAction(SET_OFFERINGS);
+export const setSearchValue = createAction(SET_SEARCH_VAL);
+export const setSearchResult = createAction(SET_SEARCH_RES);
