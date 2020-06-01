b8721643183d9cf0136f632762b38f6278274bef
Shawn Sui <ruihua.sui@gmail.com>
2020-06-01 02:05:42 -0500

    Update AutoComplete and Examples

--- src/layout/CTForm/AutoComplete/index.js
+++ src/layout/CTForm/AutoComplete/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MuiAutocomplete from '@material-ui/lab/Autocomplete';
 import { Input } from '../Input';
@@ -15,21 +16,37 @@ export function AutoComplete(props) {
     ...inputProps
   } = props;
 
-  const renderInput = (params) => (
-    <Input {...params} {...inputProps} />
-  );
+  let option = _.find(options, { value }) || {};
+
+  const [inputVal, setInputVal] = useState(option.text);
 
   const handleChange = (event, newOption) => {
-    if (typeof onChange === 'function') {
+    if (typeof onChange === 'function' && newOption) {
       onChange(newOption.value);
     }
   };
 
+  const handleInputChange = (event) => {
+    setInputVal(event ? event.target.value : '');
+  };
+
+  useEffect(() => {
+    setInputVal(option.text || '');
+  }, [value]);
+
+  const renderInput = (params) => (
+    <Input {...params} {...inputProps} />
+  );
+
   return (
     <MuiAutocomplete
+      autoHighlight
+      disableClearable
       value={value}
+      inputValue={inputVal}
       options={options}
-      getOptionLabel={(option) => option.text}
+      onInputChange={handleInputChange}
+      getOptionLabel={(opt) => opt.text}
       onChange={handleChange}
       renderInput={renderInput}
     />
--- src/layout/CTForm/ExampleUsage.js
+++ src/layout/CTForm/ExampleUsage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   CTFragment,
   CTForm, 
@@ -11,163 +11,196 @@ import {
   CTAutoComplete
 } from 'layout';
 
+function BasicForm() {
+  const [inputVal, setInputVal] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleInputChange = ({ target: { value }}) => setInputVal(value);
+  const handleCheckChange = ({ target: { checked }}) => setIsChecked(checked);
+
+  return (
+    <CTForm
+      collapsible
+      padding={[10, 35]} 
+      id="ctform-basics" 
+      heading="Basic Usage" 
+      details="Basic usage of the CTForm, includes CTInput, CTCheckbox and etc."
+      onSave={() => 1}
+      onCancel={() => 1}
+    >
+      {/* Input & Textarea */}
+      <CTFormHeading>Input & Textarea</CTFormHeading>
+
+      <CTFormRow>
+        <CTInput
+          required
+          id="course-number"
+          label="Course Number"
+          placeholder="Course Number"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+
+        <CTInput
+          error
+          id="course-name"
+          label="Input Field"
+          helpText="This is the course name"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+      </CTFormRow>
+
+      <CTFormRow>
+        <CTInput 
+          textarea
+          id="course-description"
+          defaultValue={`The description for System Programming \nCan have multiples`}
+          label="A Textarea Example"
+        />
+      </CTFormRow>
+
+      {/* Instruction box */}
+      <CTFormHeading>Instruction box</CTFormHeading>
+
+      <CTFormHelp title="Help">
+        You can write a instruction here
+      </CTFormHelp>
+
+      <CTFormRow padding={[0, 10]}>
+        <CTInput
+          required
+          underlined
+          id="course-number-2"
+          label="Underlined Input"
+          placeholder="Underlined Input"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+
+        <CTInput
+          error
+          underlined
+          id="course-name-2"
+          label="Course Name"
+          helpText="The input can be underlined"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+      </CTFormRow>
+
+
+      {/* Checkbox */}
+      <CTFormHeading>Checkbox</CTFormHeading>
+
+      <CTFormRow padding={[0, 10]}>
+        <CTCheckbox 
+          id="log-event"
+          legend="An checkbox example"
+          label="Log student events"
+          checked={isChecked}
+          onChange={handleCheckChange}
+        />
+      </CTFormRow>
+
+      <CTFormRow padding={[0, 10]}>
+        <CTCheckbox
+          error
+          id="log-event-error"
+          legend="An checkbox example with error"
+          label="Log student events"
+          helpText="An error occurs"
+          checked={!isChecked}
+          onChange={handleCheckChange}
+        />
+      </CTFormRow>
+    </CTForm>
+  );
+}
+
+function AdvancedForm() {
+  const [option, setOption] = useState('opt-1');
+  const handleSelect = ({ target: { value }}) => setOption(value);
+
+  const exampleOptions = [
+    {value: 'opt-1', text: 'Computer Science'},
+    {value: 'opt-2', text: 'Mathematics'},
+    {value: 'opt-3', text: 'Business', description: 'A description to option 3'},
+    {value: 'opt-4', text: 'Statistics', description: 'A description to option 4'}
+  ];
+
+  return (
+    <CTForm
+      expanded
+      collapsible
+      padding={[10, 35]}
+      id="ctform-sel"
+      heading="Selections"
+      details="Advacted usage of the CTForm, includes Select and AutoComplete."
+      onSaveButtonText="Create" 
+      onSave={() => 1}
+      onCancel={() => 1}
+    >
+      {/* Selection */}
+      <CTFormHeading>Selection</CTFormHeading>
+
+      <CTFormRow>
+        <CTSelect
+          id="sel-1"
+          label="Selection"
+          defaultValue="opt-1"
+          options={exampleOptions}
+          value={option}
+          onChange={handleSelect}
+        />
+
+        <CTSelect
+          underlined
+          error
+          id="sel-2"
+          label="Underlined Selection"
+          defaultValue="opt-1"
+          helpText="There is an error occurred"
+          options={exampleOptions}
+          value={option}
+          onChange={handleSelect}
+        />
+      </CTFormRow>
+
+
+      {/* Auto Complete */}
+      <CTFormHeading>Auto Complete</CTFormHeading>
+
+      <CTFormRow>
+        <CTAutoComplete 
+          id="auto-complete-example-1"
+          label="Auto complete example"
+          options={exampleOptions}
+          value={option}
+          onChange={(val) => setOption(val)}
+        />
+
+        <CTAutoComplete
+          underlined
+          id="auto-complete-example-2"
+          label="Underlined auto complete"
+          options={exampleOptions}
+          value={option}
+          onChange={(val) => setOption(val)}
+        />
+      </CTFormRow>
+    </CTForm>
+  );
+}
+
 /**
  * The basic usage of the CTForm and its components
  */
 export function CTFormExampleUsage() {
-  const alertValue = ({ target: { value }}) => alert(value);
-
-  const exampleOptions = [
-    {value: 'opt-1', text: 'Option 1'},
-    {value: 'opt-2', text: 'Option 2'},
-    {value: 'opt-3', text: 'Option 3', description: 'A description to option 3'},
-    {value: 'opt-4', text: 'Option 4', description: 'A description to option 4'}
-  ];
-
   return (
     <CTFragment>
-      <CTForm
-        collapsible
-        padding={[10, 35]} 
-        id="ctform-basics" 
-        heading="Basic Usage" 
-        details="Basic usage of the CTForm, includes CTInput, CTCheckbox and etc."
-        onSave={() => 1}
-        onCancel={() => 1}
-      >
-        {/* Input & Textarea */}
-        <CTFormHeading>Input & Textarea</CTFormHeading>
-
-        <CTFormRow>
-          <CTInput
-            required
-            id="course-number"
-            label="Course Number"
-            placeholder="Course Number"
-          />
-
-          <CTInput
-            error
-            id="course-name"
-            defaultValue="System Programming"
-            label="Course Name"
-            helpText="This is the course name"
-          />
-        </CTFormRow>
-
-        <CTFormRow>
-          <CTInput 
-            textarea
-            id="course-description"
-            defaultValue={`The description for System Programming \nCan have multiples`}
-            label="A Textarea Example"
-          />
-        </CTFormRow>
-
-        {/* Instruction box */}
-        <CTFormHeading>Instruction box</CTFormHeading>
-
-        <CTFormHelp title="Help">
-          You can write a instruction here
-        </CTFormHelp>
-
-        <CTFormRow padding={[0, 10]}>
-          <CTInput
-            required
-            underlined
-            id="course-number-2"
-            label="Underlined Input"
-            placeholder="Underlined Input"
-          />
-
-          <CTInput
-            error
-            underlined
-            id="course-name-2"
-            defaultValue="System Programming"
-            label="Course Name"
-            helpText="The input can be underlined"
-          />
-        </CTFormRow>
-
-
-        {/* Checkbox */}
-        <CTFormHeading>Checkbox</CTFormHeading>
-
-        <CTFormRow padding={[0, 10]}>
-          <CTCheckbox 
-            id="log-event"
-            legend="An checkbox example"
-            label="Log student events"
-          />
-        </CTFormRow>
-
-        <CTFormRow padding={[0, 10]}>
-          <CTCheckbox 
-            checked 
-            error
-            id="log-event-error"
-            legend="An checkbox example with error"
-            label="Log student events"
-            helpText="An error occurs"
-          />
-        </CTFormRow>
-      </CTForm>
-
-      <CTForm
-        expanded
-        collapsible
-        padding={[10, 35]}
-        id="ctform-sel"
-        heading="Selections"
-        details="Advacted usage of the CTForm, includes Select and AutoComplete."
-        onSaveButtonText="Create" 
-        onSave={() => 1}
-        onCancel={() => 1}
-      >
-        {/* Selection */}
-        <CTFormHeading>Selection</CTFormHeading>
-
-        <CTFormRow>
-          <CTSelect
-            id="sel-1"
-            label="Selection"
-            defaultValue="opt-1"
-            options={exampleOptions}
-            onChange={alertValue}
-          />
-
-          <CTSelect
-            underlined
-            error
-            id="sel-2"
-            label="Underlined Selection"
-            defaultValue="opt-1"
-            helpText="There is an error occurred"
-            options={exampleOptions}
-            onChange={alertValue}
-          />
-        </CTFormRow>
-
-
-        {/* Auto Complete */}
-        <CTFormHeading>Auto Complete</CTFormHeading>
-
-        <CTFormRow>
-          <CTAutoComplete 
-            label="Auto complete example"
-            options={exampleOptions}
-            onChange={(val) => alert(val)}
-          />
-
-          <CTAutoComplete
-            underlined
-            label="Underlined auto complete"
-            options={exampleOptions}
-            onChange={(val) => alert(val)}
-          />
-        </CTFormRow>
-      </CTForm>
+      <BasicForm />
+      <AdvancedForm />
     </CTFragment>
   );
 }
