25196dcefc5cf5fdae26ce9461ec492e555c4275
davidlin <davidlinyiyang@gmail.com>
2020-06-03 22:08:49 -0500

    #126 initial

--- src/screens/Instructor/NewCourse/index.js
+++ src/screens/Instructor/NewCourse/index.js
@@ -1,12 +1,97 @@
-import React, { Component } from 'react';
-import { CTLayout } from 'layout';
+import React, { Component, useState } from 'react';
+import { CTLayout ,
+  CTFragment,
+  CTForm, 
+  CTFormHeading, 
+  CTFormHelp,
+  CTFormRow, 
+  CTInput,
+  CTSelect,
+  CTCheckbox,
+  CTAutoComplete
+} from 'layout';
+
 import { CTFormExampleUsage } from 'layout/CTForm/ExampleUsage';
 import { api } from 'utils';
 
+export function NewCourseForm() {
+  const [inputVal, setInputVal] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+  
+  const [option, setOption] = useState('opt-1');
+
+  const handleSelect = ({ target: { value }}) => setOption(value);
+
+  const handleInputChange = ({ target: { value }}) => setInputVal(value);
+  const handleCheckChange = ({ target: { checked }}) => setIsChecked(checked);
+
+  return (
+    <CTForm
+      heading="Information"
+      padding={[10, 35]} 
+      id="ctform-basics" 
+      onSave={() => 1}
+      onCancel={() => 1}
+    >
+      <CTFormRow>
+        <CTInput
+          required
+          id="course-number"
+          label="Course Number"
+          placeholder="Course Number"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+        <CTInput
+          required
+          id="section-name"
+          label="Section Name"
+          placeholder="Section Name"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+      </CTFormRow>
+      <CTFormRow>
+        <CTInput
+          required
+          id="course-name"
+          label="Course Name"
+          placeholder="Course Name"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+      </CTFormRow>
+      
+      <CTFormRow>
+        <CTInput
+          required
+          id="section-name"
+          label="Section Name"
+          placeholder="Section Name"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+        <CTSelect
+          underlined
+          id="sel-term"
+          label="Select Term"
+          defaultValue="Select Term"
+          options={[{value: 'opt-1', text: 'Fall 2019'},
+          {value: 'opt-2', text: 'Spring 2020'},
+          {value: 'opt-3', text: 'Fall 2020'},
+          {value: 'opt-4', text: 'Spring 2021'}]}
+          value={option}
+          onChange={handleSelect}
+        />
+      </CTFormRow>
+    </CTForm>
+  );
+}
 export class NewCourse extends Component {
   componentDidMount() {
     api.contentLoaded();
   }
+  
 
   render() {
     const layoutProps = CTLayout.createProps({
@@ -24,7 +109,7 @@ export class NewCourse extends Component {
 
     return (
       <CTLayout {...layoutProps}>
-        <CTFormExampleUsage />
+        <NewCourseForm />
       </CTLayout>
     )
   }
