22e2b6238ceba1f0566fa1ab443be5d18b1a1b49
davidlin <davidlinyiyang@gmail.com>
2020-06-04 15:20:42 -0500

    #126 delete redux

--- src/redux/instructor/new-course/new-course.state.js
+++
@@ -1,9 +0,0 @@
-export const initialState = {
-  courseNumber: '',
-  courseName: '',
-  term: '',
-  sectionName: '',
-  accessType: '',
-  description: '',
-  logEvent: false,
-};
--- src/screens/Instructor/NewCourse/controllers/index.js
+++ src/screens/Instructor/NewCourse/controllers/index.js
@@ -1,3 +1,3 @@
-export { myCoursesStore, connectWithRedux } from 'redux/instructor/my-courses';
+export { newCoursesStore, connectWithRedux } from 'redux/instructor/new-courses';
 
 export { setup } from './setup';
