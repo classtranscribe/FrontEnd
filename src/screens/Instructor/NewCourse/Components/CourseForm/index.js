8849c61fcf4c69641e7c998f023a4cc92a36ea13
davidlin <davidlinyiyang@gmail.com>
2020-06-04 23:42:43 -0500

    #126 course name and section name

--- src/screens/Instructor/NewCourse/Components/CourseForm/index.js
+++ src/screens/Instructor/NewCourse/Components/CourseForm/index.js
@@ -15,9 +15,11 @@ import { CTFormExampleUsage } from 'layout/CTForm/ExampleUsage';
 import { api } from 'utils';
 import './index.scss';
 
-export default function CourseForm() {
+export function CourseForm() {
   const [inputVal, setInputVal] = useState('');
   const [isChecked, setIsChecked] = useState(false);
+
+  
   
   const [option, setOption] = useState('opt-1');
 
@@ -25,59 +27,55 @@ export default function CourseForm() {
 
   const handleInputChange = ({ target: { value }}) => setInputVal(value);
   const handleCheckChange = ({ target: { checked }}) => setIsChecked(checked);
-
+  const handleSave = () => 1;
+  const handleCancel = () => 1;
+  const [courseName, setcourseName] = useState('');
+  const setCourseName = ({ target: { value }}) => setcourseName(value);
+  const [sectionName, setsectionName] = useState('');
+  const setSectionName = ({ target: { value }}) => setsectionName(value);
   return (
     <CTForm
       heading="Information"
       padding={[10, 35]} 
       id="ctform-basics" 
-      onSave={() => 1}
-      onCancel={() => 1}
+      onSave={handleSave}
+      onSaveButtonText="Create" 
+      onCancel={handleCancel}
     >
-      <CTFormRow>
-        <CTInput
-          required
-          id="course-number"
-          label="Course Number"
-          placeholder="Course Number"
-          value={inputVal}
-          onChange={handleInputChange}
-        />
-        <CTInput
-          required
-          id="section-name"
-          label="Section Name"
-          placeholder="Section Name"
-          value={inputVal}
-          onChange={handleInputChange}
-        />
-      </CTFormRow>
       <CTFormRow>
         <CTInput
           required
           id="course-name"
           label="Course Name"
           placeholder="Course Name"
-          value={inputVal}
-          onChange={handleInputChange}
+          value={courseName}
+          onChange={setCourseName}
         />
-      </CTFormRow>
-      
-      <CTFormRow>
         <CTInput
           required
           id="section-name"
           label="Section Name"
           placeholder="Section Name"
-          value={inputVal}
-          onChange={handleInputChange}
+          value={sectionName}
+          onChange={setSectionName}
         />
+      </CTFormRow>
+      <CTFormRow>
         <CTSelect
           underlined
           id="sel-term"
           label="Select Term"
           defaultValue="Term"
-          options={[api.getTermById(5)]}
+          options={[api.getTermById(0)]}
+          value={option}
+          onChange={handleSelect}
+        />
+        <CTSelect
+          underlined
+          id="vis-type"
+          label="Visibility"
+          defaultValue="Visibility"
+          options={[api.getTermById(0)]}
           value={option}
           onChange={handleSelect}
         />
---
+++ src/screens/Instructor/NewCourse/Components/index.js
@@ -0,0 +1,1 @@
+export { CourseForm } from './CourseForm';
