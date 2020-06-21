60dbdc8260e695fab2dd3d72eddf711630d43e82
Shawn Sui <ruihua.sui@gmail.com>
2020-06-21 17:17:24 -0500

    Use prismjs to highlight code in md previewer

--- src/components/CTMarkdown/MarkdownPreviewer/index.js
+++ src/components/CTMarkdown/MarkdownPreviewer/index.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { html } from 'utils';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
 import './index.scss';
 
 /**
  * An Markdown Preview converter
  */
 function MarkdownPreviewer(props) {
-  const { value, className, parseMarkdown } = props;
-  const previewHTML = typeof parseMarkdown === 'function'
-                    ? parseMarkdown(value) 
-                    : html.markdown(value);
+  const {
+    value = '',
+    className,
+    children,
+    parseMarkdown
+  } = props;
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [])
+  
 
   const previewClasses = cx('ct-md', 'preview', className);
+  const htmlClasses = cx('ct-md', 'preview-html');
+  let previewElement = null;
+  if (value) {
+    const previewHTML = typeof parseMarkdown === 'function'
+                      ? parseMarkdown(value) 
+                      : html.markdown(value);
+
+    const previewProps = {
+      className: htmlClasses,
+      dangerouslySetInnerHTML: { __html: previewHTML }
+    };
+
+    previewElement = <div {...previewProps} />;
+  } else {
+    previewElement = (
+      <div className={htmlClasses}>
+        {children}
+      </div>
+    );
+  }
 
   return (
     <div className={previewClasses}>
-      <div className="ct-md preview-html" dangerouslySetInnerHTML={{ __html: previewHTML }} />
+      {previewElement}
     </div>
   );
 }
@@ -28,6 +57,9 @@ MarkdownPreviewer.propTypes = {
 
   /** Additional Classes */
   className: PropTypes.string,
+
+  /** The Markdown parsed html content */
+  children: PropTypes.node,
 
   /** Custom parseMarkdown function */
   parseMarkdown: PropTypes.func
