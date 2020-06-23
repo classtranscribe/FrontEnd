aed6af9bcde3673b31ec523ac67c8fa43f4db69c
rongni <rni4@illinois.edu>
2020-06-22 19:26:10 -0500

    playlist_name

---
+++ src/screens/Instructor/NewPlaylist/Components/NewPlaylistForm/PlaylistName.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { CTFragment, CTFormHeading, CTFormRow, CTInput, CTSelect, CTCheckbox } from 'layout';
+import { api, util, user } from 'utils';
+
+export function PlaylistName() {
+  const [playlistName, setPlaylistName] = useState('');
+  const handlePlaylistNameChange = ({ target: { value } }) => setPlaylistName(value);
+  return (
+    <CTFragment>
+      <CTFormHeading>PLAYLIST NAME</CTFormHeading>
+      <CTInput
+        required
+        id="playlist-name"
+        label="Playlist Name"
+        placeholder="Playlist Name"
+        value={playlistName}
+        onChange={handlePlaylistNameChange}
+      />
+    </CTFragment>
+  );
+}
