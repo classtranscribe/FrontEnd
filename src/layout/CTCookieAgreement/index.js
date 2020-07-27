b405d639cbbd682021fce6284b4e481f94024205
aparekh5 <aryamanparekh12@gmail.com>
2020-07-27 23:11:53 +0530

    Converted the div button to a  Button Base Component
    Shifted button styling from index.scss to styles.js

--- src/layout/CTCookieAgreement/index.js
+++ src/layout/CTCookieAgreement/index.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import Dialog from '@material-ui/core/Dialog';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import { Link } from 'react-router-dom';
 import { SignInMenu } from '../CTNavHeader/NavHeaderMenu/SignInMenu';
+import { useStyles } from './styles';
 import './index.scss';
 
 export const AGREEMENT_ACCEPTED_KEY = 'class_transcribe_agreement_accepted';
@@ -10,6 +12,7 @@ function CTCookieAgreement() {
   // Set true to false for testing purposes
   const [dialogOpen, setDialogOpen] = useState(!(localStorage.getItem(AGREEMENT_ACCEPTED_KEY) === 'true'));
   const [anchorEl, setAnchorEl] = useState(null);
+  const styleClasses = useStyles();
 
   const handleAcceptLoginClick = (e) => {
     localStorage.setItem(AGREEMENT_ACCEPTED_KEY, 'true');
@@ -50,43 +53,43 @@ function CTCookieAgreement() {
           By continuing you agree to the <Link to='/policy/acceptableUse'> acceptable_use policy</Link> and <Link to='/policy/cookie'>cookie policy</Link>.
         </p>
         <div id="cookie-agreement-btn-grp">
-          <div
-            className="cookie-agreement-btn"
+          <ButtonBase
+            className={styleClasses.button}
             onClick={handleAcceptLoginClick}
             role='button'
             tabIndex='0'
           >
-            <h3>
+            <h3 className='cookie-agreement-btn-h3'>
               Accept & Login
             </h3>
-            <p>
+            <p className='cookie-agreement-btn-p'>
               Access all Eligible Videos and Courses
             </p>
-          </div>
-          <div
-            className="cookie-agreement-btn"
+          </ButtonBase>
+          <ButtonBase
+            className={styleClasses.button}
             onClick={handleAcceptSkipLogin}  
             role='button'
             tabIndex='0'
           >
-            <h3>
+            <h3 className='cookie-agreement-btn-h3'>
               Accept & Skip Login
             </h3>
-            <p>
+            <p className='cookie-agreement-btn-p'>
               Access only Public Courses
             </p>
-          </div>
-          <div
-            className="cookie-agreement-btn"
+          </ButtonBase>
+          <ButtonBase
+            className={styleClasses.button}
             onClick={handleCloseBrowserWindow}  
             role='button'
             tabIndex='0'
           >
-            <h3>
+            <h3 className='cookie-agreement-btn-h3'>
               Decline and Close Window
             </h3>
-            
-          </div>
+            <p> </p>
+          </ButtonBase> 
         </div>
 
         <SignInMenu 
---
+++ src/layout/CTCookieAgreement/styles.js
@@ -0,0 +1,24 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme) => ({
+  button: {
+    background: '#328383',
+    marginLeft : '10px',
+    marginRight : '10px',
+    marginTop: '2px',
+    marginBottom: '1px', 
+    border : 0,
+    borderRadius: '3px',
+    color: 'white',
+    height: '60px',
+    padding: '10px 30px',
+    cursor: 'pointer',
+    display : 'flex',
+    flexDirection : 'column',
+    alignItems: 'start',
+    [theme.breakpoints.down('xs')] : {
+      height: '50px',
+      padding: '10px 20px',
+    },
+    }
+}));
