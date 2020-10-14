cc4eef5a3b572eb3f63929368deeea568ba5bbe6
Shawn Sui <ruihua.sui@gmail.com>
2020-10-13 20:25:03 -0500

    Try build the new EPub page from scratch

--- src/screens/EPub/controllers/file-builders/file-templates/epub/mimetype.js
+++
@@ -1,1 +0,0 @@
-export default 'application/epub+zip';
--- src/screens/EPub/controllers/file-builders/index.js
+++
@@ -1,5 +0,0 @@
-export { LoadImageError } from './EPubParser';
-
-export { default as HTMLFileBuilder } from './HTMLFileBuilder';
-export { default as EPubFileBuilder } from './EPubFileBuilder';
-export { default as ScreenshotsBuilder } from './ScreenshotsBuilder';
