ad1bb28d312f60fa52e56946171ca6de5c807053
Shawn Sui <ruihua.sui@gmail.com>
2020-10-27 03:22:02 -0500

    Remove old CTEPubGenerator component

--- src/components/CTEPubGenerator/controllers/structs/index.js
+++
@@ -1,4 +0,0 @@
-export { default as EPubImageData } from './EPubImageData';
-export { default as EPubChapterData } from './EPubChapterData';
-export { default as EPubSubChapterData } from './EPubSubChapterData';
-export { default as EPubData, EPubDataValidationError } from './EPubData';
