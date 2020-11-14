5b553434de457b25b212db8ae865d283f126553d
Shawn Sui <ruihua.sui@gmail.com>
2020-11-14 17:44:31 -0600

    Remove outdated maintenance message

--- src/screens/Home/components/MaintenanceMesg/index.js
+++ src/screens/Home/components/MaintenanceMesg/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { CTFragment, CTText } from 'layout';
 
-function MaintenanceMesg() {
-  return (
+function MaintenanceMesg({ message }) {
+  return message ? (
     <CTFragment styles={{ background: 'teal' }} justConCenter padding="15">
       <CTText white bold size="medium">
-        ClassTranscribe will be unavailable for a few hours 
-        Friday afternoon CT (Nov 13) due to network maintenance
+        {message}
       </CTText>
     </CTFragment>
-  );
+  ) : null;
 }
 
 export default MaintenanceMesg;
