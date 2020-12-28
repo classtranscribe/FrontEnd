e6fc6f813ed1b4fddb9a4af20e59c45dae9b329c
Zhenyu Mao <maozhenyu@foxmail.com>
2020-12-28 06:02:15 -0600

    new homepage

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
-import { withReduxProvider } from 'redux/redux-provider';
+import { connect } from 'dva';
 import { CTLayout, CTLoadable, altEl, makeEl } from 'layout';
 import { ARRAY_INIT } from 'utils/constants';
-import { homeStore, connectWithRedux, home } from './controllers';
 import { Placeholder, SectionList, CourseFilter, MaintenanceMesg } from './components';
 
 class HomeWithRedux extends Component {
   constructor(props) {
     super(props);
-
-    home.state.init(props);
   }
 
   componentDidMount() {
-    home.ctrl.setupHomepage();
+    // home.ctrl.setupHomepage(); no need
   }
 
   render() {
-    const { sections } = this.props;
+    const { sections, hasDepartmentSections } = this.props;
     const layoutProps = CTLayout.createProps({
       transition: true,
       responsive: true,
       footer: true
     });
-
     const loading = sections === ARRAY_INIT;
     const loaderElement = makeEl(Placeholder);
-    const sectionElement = altEl(SectionList, !loading, { sections });
+    const sectionElement = altEl(SectionList, !loading, { sections, hasDepartmentSections });
     const filterElement = altEl(CourseFilter, !loading);
-
+    console.log('render');
     return (
       <CTLayout {...layoutProps}>
         <MaintenanceMesg message="" />
@@ -41,10 +37,6 @@ class HomeWithRedux extends Component {
   }
 }
 
-export const Home = withReduxProvider(
-  HomeWithRedux,
-  homeStore,
-  connectWithRedux,
-  ['sections'],
-  ['all']
-);
+export const Home = connect(({ home, loading }) => ({
+  ...home
+}))(HomeWithRedux);
