0c09289747ed3e4e7ee907449b2f0140dd71b615
mgautam2 <mgautam2@illinois.edu>
2021-03-05 22:35:53 -0600

    Fixxed issue with yarn.lock

--- changeBackend.js
+++ changeBackend.js
@@ -23,21 +23,21 @@ function change(data, url) {
 }
 
 /**
- * gets config data from url and adds new env var
- * before saving config file in /public dir
- *
- * @param {string} url    url of chosen backend
+ * fetches config file
+ * @param {string} url     url of chosen backend
  */
-async function setBackend(url) {
-  let data = await axios.get(`${backendList["dev"]}/config.js`)
+async function fetchConfig(url) {
+  let data = await axios.get(`${url}/config.js`)
                   .then(({data}) => data)
                   .catch(err => {})
-  if (!data) {
-    console.log(chalk.red.bold(`Could not connect to server ${url}`))
-    return
-  }
-  data = change(data, url);
-    
+  return data;
+}
+
+/**
+ * saves data to "./public/config"
+ * @param {string} data     url of chosen backend
+ */
+function saveToPublicDir(data, url) {
   try {
     fs.writeFileSync("./public/config.js", data)
     console.log(chalk.green(`Backend : ${chalk.bold(url)}`))
@@ -47,23 +47,67 @@ async function setBackend(url) {
   }
 }
 
+
+/**
+ * gets config data from url and adds new env var
+ * before saving config file in /public dir
+ * @param {string} url    url of chosen backend
+ */
+async function setBackend(url) {
+  let data =  await fetchConfig(url)
+  
+  if (!data) {
+    console.log(chalk.red.bold(`Could not connect to server ${url} `))
+    return
+  }
+  data = change(data, url);
+  saveToPublicDir(data, url);
+}
+
+
+/**
+ * gets config data from dev and sets backend to custom address
+ * before saving config file in /public dir
+ * @param {string} url    url of chosen backend
+ */
+async function setCustomBackend({localhost : url}) {
+  
+  if (!url.startsWith("http://localhost")) {
+    console.log(chalk.red(`The server address must be complete \ni.e. ${chalk.bold("https://localhost[...]")}`))
+    return;
+  }
+  
+  let data = await fetchConfig(backendList['dev'])
+  
+  if (!data) {
+    console.log(chalk.red.bold(`Could not connect to server ${backendList['dev']} `))
+    return
+  }
+  data = change(data, url);
+  saveToPublicDir(data, url);
+}
+
 /**
  * processs command line arguments to select 
  * dev or production server
  */
 function processArgs() {
   program
-    .option('-p --production', backendList.production )
+    .option('-p --production', `${backendList.production} (Currently not working)` )
     .option('-d --dev', backendList.dev)
+    .option('-l --localhost <string>', "complete Localhost address")
 
   program.parse(process.argv);
   let backend =  Object.keys(program.opts())
-
+  
   if (backend.length !== 1) {
     console.log(chalk.red(`Invalid args \nType : ${chalk.bold("yarn backend -h")}`))
     return
   }
-  setBackend(backendList[backend[0]]);
+  if (backend[0] === "localhost")
+    setCustomBackend(program.opts())
+  else 
+    setBackend(backendList[backend[0]]);
 }
 
 /**
