5c9e0e82ce83240092d51eabf1f052a3118d84ad
Harsh Deep <hrshdeep51@gmail.com>
2021-07-01 08:46:22 -0500

    add better default title

--- src/screens/Watch/live_test.jsx
+++ src/screens/Watch/live_test.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+// import { useEffect } from 'react';
 import CTPlayer from 'components/CTPlayer';
 import { uurl } from 'utils/use-url';
 import { Transcriptions } from './Components';
@@ -13,9 +14,10 @@ function LiveTest(_props) {
         isLive: true, // if this source is live
         videos: [{
             useHls: true,
-            srcPath1: videosrc
+            srcPath1: videosrc,
             // srcPath1: 'https://klive.kaltura.com/s/dc-1/live/hls/p/1359391/e/1_23mx1syi/sd/6000/t/5cWuUSMATwMSDUQSIdCbnw/index-s32.m3u8?__hdnea__=st=1618984738~exp=1619071138~acl=/s/dc-1/live/hls/p/1359391/e/1_23mx1syi/sd/6000/t/5cWuUSMATwMSDUQSIdCbnw/index-s32.m3u8*~hmac=f2462a504f3b020d2be1862aaab876b93a77b1f8f682a757215e6a93cea8b898'
-        }]
+        }],
+        mediaName: 'Live Meeting Cast', /* TODO: Pull out titles from somewhere, hls doesn't have titles */
     };
     // https://hls-js.netlify.app/demo/
     return (
