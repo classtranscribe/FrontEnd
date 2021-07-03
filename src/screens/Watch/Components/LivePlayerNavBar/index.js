948bd6c2bfd8bdacb017bf2384a86874a5a76b6e
shuhaok2 <shuhaok2@illinois.edu>
2021-07-03 22:21:19 +0800

    fix lint errors

--- src/screens/Watch/Components/LivePlayerNavBar/index.js
+++ src/screens/Watch/Components/LivePlayerNavBar/index.js
@@ -3,22 +3,22 @@ import Navbar from 'react-bootstrap/Navbar';
 import { UISlogo } from '../../../../assets/images';
 
 export function LivePlayerNavBar() {
-    return (
-        <Navbar bg="light">
-            <Navbar.Brand href="https://www.bot.uillinois.edu/">
-                <img
-                    src={UISlogo}
-                    width="30"
-                    height="42"
-                    alt="Board of Trustees"
-                />
-                Board of Trustees
-            </Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text>
-                    <a href="https://www.uis.edu/" style={{ textDecoration: 'none' }}>University of Illinois Springfield </a>
-                </Navbar.Text>
-            </Navbar.Collapse>
-        </Navbar>
+  return (
+    <Navbar bg="light">
+      <Navbar.Brand href="https://www.bot.uillinois.edu/">
+        <img
+          src={UISlogo}
+          width="30"
+          height="42"
+          alt="Board of Trustees"
+        />
+        Board of Trustees
+      </Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          <a href="https://www.uis.edu/" style={{ textDecoration: 'none' }}>University of Illinois Springfield </a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
     );
 }
