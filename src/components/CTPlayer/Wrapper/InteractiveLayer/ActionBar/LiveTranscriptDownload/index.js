2c00d508de793331d8057b2af2fc678fe47c55b0
Harsh Deep <hrshdeep51@gmail.com>
2021-07-22 10:40:09 -0500

    convert live transcript download to text

--- src/components/CTPlayer/Wrapper/InteractiveLayer/ActionBar/LiveTranscriptDownload/index.js
+++ src/components/CTPlayer/Wrapper/InteractiveLayer/ActionBar/LiveTranscriptDownload/index.js
@@ -7,34 +7,51 @@ function LiveTranscriptDownloadWithRedux(props) {
     const {media, transcript} = props;
 
     // filename safe based off: https://stackoverflow.com/questions/8485027/javascript-url-safe-filename-safe-string
-    const getFileName = (mediaName) => {
+    const generateFileName = (mediaName) => {
       const cleanMediaName = mediaName.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-      const fileName = `${cleanMediaName}_transcript.vtt`;
+      const fileName = `${cleanMediaName}_transcript`;
       return fileName;
     };
 
     // https://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript
     const timestampFormat = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 12)
 
-    const createVttFile = () => {
-      const header = "WEBVTT\n";
+    // start and end times are borked, so disabling this for the time being
+    // const createVttFile = () => {
+    //   const header = "WEBVTT\n";
+    //   const lines = transcript.map(vtcue => {
+    //     if (vtcue === undefined || vtcue.text === undefined || vtcue.text === "") {
+    //       return "";
+    //     }
+        
+    //     const cueStart = timestampFormat(vtcue.startTime);
+    //     const cueEnd = timestampFormat(vtcue.endTime)
+    //     return `\n${cueStart} --> ${cueEnd}\n${vtcue.text}\n`;        
+    //   });
+    //   const textContent = header + lines.join("");
+    //   return [textContent, "vtt"];
+    // };
+
+    const createTextFile = () => {
       const lines = transcript.map(vtcue => {
-        const cueStart = timestampFormat(vtcue.startTime);
-        const cueEnd = timestampFormat(vtcue.endTime)
-        return `\n${cueStart} --> ${cueEnd}\n${vtcue.text}\n`;
+        if (vtcue === undefined || vtcue.text === undefined || vtcue.text === "") {
+          return "";
+        }
+
+        return `${vtcue.text}\n`;
       });
-      const textContent = header + lines.join("");
-      return textContent;
-    };
+      const textContent = lines.join("");
+      return [textContent, "txt"];
+    }
 
     const isTranscriptEmpty = () => transcript.length === 1 && transcript[0] === 'empty';
     const triggerDownload = () => {
         if (isTranscriptEmpty()) {
           return;
         }
-        const fileName = getFileName(media.mediaName);
-        const vttFile = createVttFile();
-        fileDownload(vttFile, fileName);
+        const [transcriptFile, ext] = createTextFile();
+        const fileName = `${generateFileName(media.mediaName)}.${ext}`;
+        fileDownload(transcriptFile, fileName);
     }
     
     return (
