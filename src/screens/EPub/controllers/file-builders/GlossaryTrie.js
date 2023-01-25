f8bb40af502c91eb1f5badb8c5c4eb231ee0eb17
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-01-25 16:05:12 -0600

    Testing glossary trie on sample text

--- src/screens/EPub/controllers/file-builders/GlossaryTrie.js
+++ src/screens/EPub/controllers/file-builders/GlossaryTrie.js
@@ -32,6 +32,11 @@ class Node
 
         return null;
     }
+
+    remove(str)
+    {
+
+    }
 }
 
 class Glossary
@@ -72,19 +77,61 @@ class Glossary
     }
 }
 
-const trie = new Glossary();
-trie.insert("abc");
-trie.insert("bc");
-trie.insert("cde");
+// let trie = new Glossary();
+// trie.insert("abc");
+// trie.insert("bc");
+// trie.insert("cde");
 
-const assert = require('assert')
+// const assert = require('assert')
 
-assert(trie.has_prefix("cd"));
-assert(!trie.has_complete("cd"));
-assert(trie.has_complete("cde"));
+// assert(trie.has_prefix("cd"));
+// assert(!trie.has_complete("cd"));
+// assert(trie.has_complete("cde"));
 
-assert(trie.has_prefix("ab"))
-assert(trie.has_prefix("abc"))
+// assert(trie.has_prefix("ab"))
+// assert(trie.has_prefix("abc"))
 
-assert(!trie.has_complete("ab"))
-assert(trie.has_complete("abc"))
+// assert(!trie.has_complete("ab"))
+// assert(trie.has_complete("abc"))
+
+const glossaryData = {
+    'hello world': '1',
+    'abc': '2',
+}
+
+console.log("==================================")
+
+const chapterText = "'hello world hello world hello world abc abc abc'";
+
+trie = new Glossary();
+
+for (const key of Object.keys(glossaryData)) {
+    trie.insert(key);
+}
+
+for(let i = 0; i < chapterText.length; i++)
+{
+    let found = false;
+    let word = '';
+
+    let itr = trie.find(chapterText[i]);
+
+    for (let j = i+1; itr && j < chapterText.length; j++) {
+        // console.log(i,j,itr);
+
+        if (itr.end) {
+            found = true;
+            word = chapterText.substring(i, j);
+            break;
+        }
+        else {
+            itr = itr.children[chapterText[j]];
+        }
+    }
+
+    if (itr && itr.end) {
+        console.log("Found at i=", i, word)
+    } else {
+        // console.log("Not found at i=",i);
+    }
+}
