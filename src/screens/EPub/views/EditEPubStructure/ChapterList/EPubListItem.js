912f0a27688d9f533bf8401c3800046e13413779
Meghana <mg76@illinois.edu>
2023-03-20 10:28:19 -0500

    attempted to remove all chapters with length less than 100

--- src/screens/EPub/views/EditEPubStructure/ChapterList/EPubListItem.js
+++ src/screens/EPub/views/EditEPubStructure/ChapterList/EPubListItem.js
@@ -20,114 +20,119 @@ function EPubListItem({
   setEPubItem,
   dispatch
 }) {
-  const imgSrc = uurl.getMediaUrl(item.image);
-  const SubCParams = { chapterIdx: chapterIndex, subChapterIdx: subChapterIndex, itemIdx: itemIndex };
-  const CParams = { chapterIdx: chapterIndex, itemIdx: itemIndex };
+  if(item.text.length > 30) {
+    const imgSrc = uurl.getMediaUrl(item.image);
+    const SubCParams = { chapterIdx: chapterIndex, subChapterIdx: subChapterIndex, itemIdx: itemIndex };
+    const CParams = { chapterIdx: chapterIndex, itemIdx: itemIndex };
 
-  const splitChapterFromSubChaptersItems = () => dispatch({
-    type: 'epub/updateEpubData', payload: {
-      action: 'splitChapterFromSubChaptersItems', payload: SubCParams
-    }
-  });
+    const splitChapterFromSubChaptersItems = () => dispatch({
+      type: 'epub/updateEpubData', payload: {
+        action: 'splitChapterFromSubChaptersItems', payload: SubCParams
+      }
+    });
 
-  const splitChapterFromChaptersItems = () => dispatch({
-    type: 'epub/updateEpubData', payload: {
-      action: 'splitChapterFromChaptersItems', payload: CParams
-    }
-  });
+    const splitChapterFromChaptersItems = () => dispatch({
+      type: 'epub/updateEpubData', payload: {
+        action: 'splitChapterFromChaptersItems', payload: CParams
+      }
+    });
 
-  const handleSplitChapter = isSubChapter
-    ? splitChapterFromSubChaptersItems
-    : splitChapterFromChaptersItems;
+    const handleSplitChapter = isSubChapter
+      ? splitChapterFromSubChaptersItems
+      : splitChapterFromChaptersItems;
 
-  const splitSubChapter = () => dispatch({
-    type: 'epub/updateEpubData', payload: {
-      action: 'splitSubChapter', payload: SubCParams
-    }
-  });
+    const splitSubChapter = () => dispatch({
+      type: 'epub/updateEpubData', payload: {
+        action: 'splitSubChapter', payload: SubCParams
+      }
+    });
 
-  const subdivideChapter = () => dispatch({
-    type: 'epub/updateEpubData', payload: {
-      action: 'subdivideChapter', payload: CParams
-    }
-  });
+    const subdivideChapter = () => dispatch({
+      type: 'epub/updateEpubData', payload: {
+        action: 'subdivideChapter', payload: CParams
+      }
+    });
 
-  // const magnifyImage = () => epub.ctrl.magnifyImage(imgSrc);
-  // const endMagnifyImage = () => epub.ctrl.endMagnifyImage();
-  const openItemDetails = () => setEPubItem(item);
+    // const magnifyImage = () => epub.ctrl.magnifyImage(imgSrc);
+    // const endMagnifyImage = () => epub.ctrl.endMagnifyImage();
+    const openItemDetails = () => setEPubItem(item);
 
 
-  const itemClass = cx('ct-epb', 'sch', 'epb-data-item', 'ct-d-c', {
-    sub: isSubChapter,
-    'padded-actions': !canSplit && !isSubChapter,
-  });
+    const itemClass = cx('ct-epb', 'sch', 'epb-data-item', 'ct-d-c', {
+      sub: isSubChapter,
+      'padded-actions': !canSplit && !isSubChapter,
+    });
 
-  const btnProps = {
-    round: true,
-    uppercase: true,
-    classNames: 'item-action-btn',
-    color: 'teal transparent'
-  };
+    const btnProps = {
+      round: true,
+      uppercase: true,
+      classNames: 'item-action-btn',
+      color: 'teal transparent'
+    };
 
-  const splitBtnElement = altEl(Button, canSplit, {
-    ...btnProps,
-    text: 'Split Chapter',
-    icon: 'unfold_more',
-    onClick: handleSplitChapter
-  });
+    const splitBtnElement = altEl(Button, canSplit, {
+      ...btnProps,
+      text: 'Split Chapter',
+      icon: 'unfold_more',
+      onClick: handleSplitChapter
+    });
 
-  const splitSChBtnElement = altEl(Button, canSplitSubChapter, {
-    ...btnProps,
-    text: 'New Sub-Chapter',
-    icon: 'subdirectory_arrow_right',
-    onClick: splitSubChapter
-  });
+    const splitSChBtnElement = altEl(Button, canSplitSubChapter, {
+      ...btnProps,
+      text: 'New Sub-Chapter',
+      icon: 'subdirectory_arrow_right',
+      onClick: splitSubChapter
+    });
 
-  const subdivideBtnElement = altEl(Button, canSubdivide, {
-    ...btnProps,
-    text: 'subdivide',
-    icon: 'subdirectory_arrow_right',
-    onClick: subdivideChapter
-  });
+    const subdivideBtnElement = altEl(Button, canSubdivide, {
+      ...btnProps,
+      text: 'subdivide',
+      icon: 'subdirectory_arrow_right',
+      onClick: subdivideChapter
+    });
+    return (
+      <div role="listitem" className={itemClass}>
+        <div className="item-actions">
+          {splitBtnElement}
+          {splitSChBtnElement}
+          {subdivideBtnElement}
+        </div>
 
+        <div
+          id={item.id}
+          role="button"
+          tabIndex="0"
+          className="ct-epb ct-d-r item-info"
+          onClick={openItemDetails}
+          aria-haspopup="true"
+          aria-controls={epub.id.epbItemViewId(item.id)}
+        >
+          <div className="item-time-con">
+            <div className="item-time">
+              {timestr.toPrettierTimeString(item.start)}
+            </div>
+            <div className="item-time">
+              {timestr.toPrettierTimeString(item.end)}
+            </div>
+          </div>
+
+          <div className="item-img-con">
+            <Image src={imgSrc} alt="screenshot" />
+          </div>
+          {/* text below must be modified to make sure text length is greater than a certain amount */}
+          <CTText line={4} className="item-text">
+            {
+              item.text
+              ||
+              <span className="text-muted"><i>No Transcriptions</i></span>
+            }
+          </CTText>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div role="listitem" className={itemClass}>
-      <div className="item-actions">
-        {splitBtnElement}
-        {splitSChBtnElement}
-        {subdivideBtnElement}
-      </div>
-
-      <div
-        id={item.id}
-        role="button"
-        tabIndex="0"
-        className="ct-epb ct-d-r item-info"
-        onClick={openItemDetails}
-        aria-haspopup="true"
-        aria-controls={epub.id.epbItemViewId(item.id)}
-      >
-        <div className="item-time-con">
-          <div className="item-time">
-            {timestr.toPrettierTimeString(item.start)}
-          </div>
-          <div className="item-time">
-            {timestr.toPrettierTimeString(item.end)}
-          </div>
-        </div>
-
-        <div className="item-img-con">
-          <Image src={imgSrc} alt="screenshot" />
-        </div>
-        <CTText line={4} className="item-text">
-          {
-            item.text
-            ||
-            <span className="text-muted"><i>No Transcriptions</i></span>
-          }
-        </CTText>
-      </div>
-    </div>
+    <div></div>
   );
 }
 
