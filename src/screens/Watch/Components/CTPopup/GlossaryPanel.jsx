09a315891cbc262126124b7fd80c7c8af3b64287
Mystri <boyouhan@gmail.com>
2023-06-16 16:47:07 -0500

    Added scrolling based on current timeframe

--- src/screens/Watch/Components/CTPopup/GlossaryPanel.jsx
+++ src/screens/Watch/Components/CTPopup/GlossaryPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import './CTPopup.scss'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
@@ -11,6 +11,17 @@ function GlossaryPanel(props) {
   const [sortMethodIndex, selectSortMethodIndex] = useState(0);
   const SORT_MODE_AZ = 0;
   const SORT_MODE_TIME = 1;
+  const NO_HIGHLIGHT = 0;
+  const FIRST_HIGHLIGHT = 1;
+  const HIGHLIGHT = 2;
+  // const scrollCallback = useCallback((element) => {
+  //   console.log(element);
+  //   if (element) {
+  //     window.scrollTo({behavior: 'smooth', top: element.offsetTop})
+  //   }
+  // }, [time]);
+  const scrollRef = useRef(null);
+ 
 
   // update todisplay of az whenever searchkeyword changes.
   useEffect(() => {
@@ -41,16 +52,24 @@ function GlossaryPanel(props) {
   // If current sort mode is time, update display whenever time or searchkeyword changes.
   useEffect(() => {
     const curstamp = parseInt(time);
-    // if the time of the glossary includes current time, we 
+    // If the time of the glossary includes current time, we 
     // should make it highlighted.
-    const toDisplay = glossaries.map(glossaryEntry => {
+    // Also, if this is the fist element we should highlight, 
+    // we mark it to make it scroll to the top of the display.
+    let firstHighlightMarked = false;
+    let toDisplay = glossaries;
+    toDisplay.sort((a, b) => a.begin - b.begin);
+    toDisplay = toDisplay.map(glossaryEntry => {
       if (glossaryEntry.begin <= curstamp && glossaryEntry.end >= curstamp) {
-        return {...glossaryEntry, highlight: true}
+        if (firstHighlightMarked) {
+          return {...glossaryEntry, highlight: HIGHLIGHT};
+        }
+        firstHighlightMarked = true;
+        return {...glossaryEntry, highlight: FIRST_HIGHLIGHT}
       }
-      return {...glossaryEntry, highlight: false}
+      return {...glossaryEntry, highlight: NO_HIGHLIGHT};
     });
     // sort the glossaries by start of timestamp.
-    toDisplay.sort((a, b) => a.begin - b.begin);
 
     setGlossariesToDisplay(glossariesToDisplay.map((list, index) => {
       if (index === SORT_MODE_TIME) {
@@ -60,6 +79,12 @@ function GlossaryPanel(props) {
     }));
   }, [time]);
 
+  // After updating the glossaries, scroll it to the appropriate position.
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({behavior: 'smooth', block: 'start'});
+    }
+  }, [glossariesToDisplay[SORT_MODE_TIME]]);
 
 
   function onSelectGlossariesOrderMode(index) {
@@ -91,7 +116,7 @@ function GlossaryPanel(props) {
 
           <ul>
             {glossariesToDisplay[SORT_MODE_AZ].map((element) => 
-              <li className='glossary-entry'>
+              <li className='glossary-entry' key={element.word}>
                 <button onClick={() => setTerm(element)}>
                   <span>
                     {element.word}
@@ -103,25 +128,34 @@ function GlossaryPanel(props) {
         </TabPanel>
         <TabPanel className='glossary-list-container'>
           <ul>
-            {glossariesToDisplay[SORT_MODE_TIME].map((element) =>
-              element.highlight 
-              ?
-                <li className='glossary-entry-highlighted'>
+            {/* Render the appropriate glossaries. */}
+            {glossariesToDisplay[SORT_MODE_TIME].map((element) => {              
+              // Highlight needed glossaries.
+              if (element.highlight !== NO_HIGHLIGHT) {
+                return (
+                  <li 
+                    className='glossary-entry-highlighted'
+                    key={element.word}
+                    // If this is the first highlighted glossary, set scrollRef, which
+                    // is necessary in order to scroll to its position.
+                    ref={element.highlight === FIRST_HIGHLIGHT ? scrollRef : undefined}
+                  >
+                    <button onClick={() => setTerm(element)}>
+                      <span>
+                        {element.word}
+                      </span>
+                    </button>
+                  </li>);
+              }
+              return (
+                <li key={element.word} className='glossary-entry'>
                   <button onClick={() => setTerm(element)}>
                     <span>
                       {element.word}
                     </span>
                   </button>
-                </li>
-              :
-              <li className='glossary-entry'>
-                <button onClick={() => setTerm(element)}>
-                  <span>
-                    {element.word}
-                  </span>
-                </button>
-              </li>
-            )}
+                </li>);
+            })}
           </ul>
         </TabPanel>
       </Tabs>
