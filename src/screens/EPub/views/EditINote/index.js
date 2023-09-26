230f5cf7c07f67cf4154acd0b04b12f54a29f5f1
Meghana <mg76@illinois.edu>
2023-09-26 11:19:48 -0500

    removed unused code

--- src/screens/EPub/views/EditINote/index.js
+++ src/screens/EPub/views/EditINote/index.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import _ from 'lodash'
-import cx from 'classnames';
 
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { CTFragment, CTHeading, CTText, altEl, useButtonStyles } from 'layout';
+import { CTFragment, CTHeading} from 'layout';
 import { connect } from 'dva'
 import Button from '@material-ui/core/Button';
 import { EPubNavigationProvider } from '../../components';
-import { epub as epubController, generateEPubGuide} from '../../controllers';
+import { epub as epubController} from '../../controllers';
 import INoteEditor from './INoteEditor';
-import Instruction from '../EditEPubStructure/Instruction';
-import EPubItemView from '../EditEPubStructure/EPubItemView';
 import QuickActionsEditNote from './QuickActionsEditNote';
 
 
@@ -20,29 +17,6 @@ function EditINote ({epub: epubData, dispatch}) {
   const dispatchScroll = _.debounce((e) => dispatch({ type: 'epub/onScroll', payload: e }), 300)
   const onScroll = (e) => dispatchScroll(e.target)
 
-  const [ePubItem, setEPubItem] = useState(null);
-  const [instExp, setInstExp] = useState(true);
-
-  const toggleInstExp = (e, newExpanded) => setInstExp(newExpanded);
-
-  useEffect(() => {
-    // show the user onboard guide if possible
-    setTimeout(() => {
-      const guide = generateEPubGuide();
-      guide.start();
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
-    if (Boolean(ePubItem) && instExp) {
-      setInstExp(false);
-    }
-  }, [ePubItem]);
-  const itemViewElem = altEl(EPubItemView, Boolean(ePubItem), {
-    item: ePubItem, setEPubItem
-  });
-  const btnStyles = useButtonStyles();
-  const btnClasses = cx(btnStyles.tealLink, 'justify-content-start');
   const [iNoteItem, setINoteItem] = useState(null);
   const [hidden, setHidden] = useState(true);
   return (
