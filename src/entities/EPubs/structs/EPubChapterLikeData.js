fd76091a27b0679946282ae225b28135b2da4f29
Lawrence Angrave <4468456+angrave@users.noreply.github.com>
2023-10-12 12:27:29 -0500

    Update EPubChapterLikeData.js
    Removed "OCR Phrases"; it doesn't mean much to most people.

--- src/entities/EPubs/structs/EPubChapterLikeData.js
+++ src/entities/EPubs/structs/EPubChapterLikeData.js
@@ -8,7 +8,7 @@ function _buildContentsFromItems(items) {
   const content = [];
   for (const item of items) {
     if (item.image) { // if there is an image 
-      const altText = item.ocrPhrases ? `OCR Phrases: ${item.ocrPhrases}` : '' // add in OCR data
+      const altText = item.ocrPhrases ? `${item.ocrPhrases}` : '' // add in OCR data
       const imageData = new EPubImageData({src: item.image, alt: altText}); 
       content.push(imageData)
     }
