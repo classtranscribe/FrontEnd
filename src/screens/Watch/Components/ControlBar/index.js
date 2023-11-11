f9917426d34c0cc7aab8bd2cd86d8acb1d14725a
Lawrence Angrave <angrave@illinois.edu>
2023-11-11 12:14:49 -0600

    Initial Multitranscription support

--- src/screens/Watch/Components/ControlBar/index.js
+++ src/screens/Watch/Components/ControlBar/index.js
@@ -14,6 +14,7 @@ import {
   PlaybackRateButton,
   ClosedCaptionButton,
   LanguagePickerButton,
+  TranscriptionPickerButton,
   AudioDescriptionButton,
   ScreenModeSettingButton,
   GlossaryButton, // May 20 Jiaxi
@@ -23,6 +24,7 @@ import VolumeControl from './VolumeControl';
 import TimeDisplay from './TimeDisplay';
 import ProgressBar from './ProgressBar';
 
+// eslint-disable-next-line complexity
 export function ControlBarWithRedux(props) {
   const { dispatch, media = {}, bulkEditing = false } = props;
   const { isTwoScreen, transcriptions } = media;
@@ -55,7 +57,10 @@ export function ControlBarWithRedux(props) {
         <ClosedCaptionButton />
         <AudioDescriptionButton />
 
-        {hasTrans && <LanguagePickerButton />}
+        {/* marked for removal in future version */}
+        {false && hasTrans && <LanguagePickerButton />}
+        
+        {hasTrans && <TranscriptionPickerButton />}
 
         {showScreenModes && <ScreenModeSettingButton isTwoScreen={isTwoScreen} />}
 
