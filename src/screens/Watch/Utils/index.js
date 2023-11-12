9c9156156a94ba206786283106f93c3e3068d31d
Lawrence Angrave <angrave@illinois.edu>
2023-11-12 14:55:26 -0600

    Allow Complex Function

--- src/screens/Watch/Utils/index.js
+++ src/screens/Watch/Utils/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 import { connect } from 'dva'
 import _, { filter, transform } from 'lodash'
 import { 
@@ -128,7 +129,11 @@ export const getVideoStyle = (options) => {
 /**
 * Function that scrolls the captions
 */
+let prevCurrentId = null;
+
 export const scrollTransToView = (id, smoothScroll = true, isTwoScreen) => {
+  // eslint-disable-next-line no-console
+  // console.log(`scrollTransToView ${id} ${smoothScroll}`)
   let capId = id;
   if (!capId) return;
   const capElem = document.getElementById(`caption-line-${capId}`);
@@ -137,12 +142,20 @@ export const scrollTransToView = (id, smoothScroll = true, isTwoScreen) => {
   const tranBox = document.getElementById('watch-trans-container');
 
   const shouldSmoothScroll = smoothScroll && tranBox.scrollTop - capElem.offsetTop < 0;
-
-  if (!shouldSmoothScroll) tranBox.style.scrollBehavior = 'auto';
-  capElem.classList.add('curr-line');
+  
+  tranBox.style.scrollBehavior = shouldSmoothScroll ? 'smooth': 'auto' ;
+  // capElem.classList.add('curr-line'); // A grep suggests this style is not defined?
+  capElem.setAttribute('current','true');
+  if(prevCurrentId && prevCurrentId !== capId) {
+    let prevElem = document.getElementById(`caption-line-${prevCurrentId}`);
+    if(prevElem) {
+      prevElem.removeAttribute('current');
+    }
+  }
+  prevCurrentId = capId;
   const scrollTop =
       window.innerWidth < 900 || !isTwoScreen ? capElem.offsetTop - 50 : capElem.offsetTop - 80;
   // if (preferControl.defaultTransView() === TRANSCRIPT_VIEW) scrollTop -= 400 NOT IMPLEMENTED
   tranBox.scrollTop = scrollTop;
-  if (!shouldSmoothScroll) tranBox.style.scrollBehavior = 'smooth';
+  // if (!shouldSmoothScroll) tranBox.style.scrollBehavior = 'smooth';
 }
