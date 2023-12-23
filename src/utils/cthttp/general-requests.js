695fe3ec7a10c0061f557e108675a4dee9e63549
Lawrence Angrave <angrave@illinois.edu>
2023-12-22 19:14:30 -0600

    Remove REACT_APP_FRONTEND_COMMIT_ENDPOINT

--- src/utils/cthttp/general-requests.js
+++ src/utils/cthttp/general-requests.js
@@ -2,14 +2,14 @@ import { cthttp } from './request';
 import { env } from '../env';
 
 // get latest git commit of FrontEnd repo
-function getLatestGitCommitData() {
-  return cthttp.request(false).get(env.frontendCommitEndpoint);
-}
+// function getLatestGitCommitData() {
+//   return cthttp.request(false).get(env.frontendCommitEndpoint);
+// }
 
-export async function getLatestGitCommitSHA() {
-  const { data } = await getLatestGitCommitData();
-  return data.sha;
-}
+// export async function getLatestGitCommitSHA() {
+//   const { data } = await getLatestGitCommitData();
+//   return data.sha;
+// }
 
 export function getFile(path) {
   return cthttp.request(false).get(path);
