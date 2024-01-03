71efd42a5e9e0fb7ebaa2f25552d0d855bb52567
Lawrence Angrave <angrave@illinois.edu>
2024-01-02 20:27:00 -0600

    Add aria-busy to loading fragments

--- src/screens/Watch/Components/Search/Placeholder.jsx
+++ src/screens/Watch/Components/Search/Placeholder.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 export default function Placeholder({ small = false }) {
   return small ? (
-    <div className="w-100 d-flex justify-content-center align-items-center px-3">
-      <div className="sk-bounce">
+    <div className="w-100 d-flex justify-content-center align-items-center px-3" aria-busy aria-label="Content not yet available">
+      <div className="sk-bounce" role="presentation">
         <div className="sk-bounce-dot" />
         <div className="sk-bounce-dot" />
       </div>
-      <div className="sk-bounce mx-1">
+      <div className="sk-bounce mx-1" role="presentation">
         <div className="sk-bounce-dot" />
         <div className="sk-bounce-dot" />
       </div>
-      <div className="sk-bounce">
+      <div className="sk-bounce" role="presentation">
         <div className="sk-bounce-dot" />
         <div className="sk-bounce-dot" />
       </div>
     </div>
   ) : (
-    <div className="w-100 d-flex justify-content-center align-items-center p-5">
-      <div className="sk-flow">
+    <div className="w-100 d-flex justify-content-center align-items-center p-5" aria-busy aria-label="Content not yet available">
+      <div className="sk-flow" role="presentation">
         <div className="sk-flow-dot" />
         <div className="sk-flow-dot" />
         <div className="sk-flow-dot" />
