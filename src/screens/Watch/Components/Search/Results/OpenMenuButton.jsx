b07021bf010010276820fb31cf252e1000acd814
Lawrence Angrave <angrave@illinois.edu>
2024-01-02 20:37:37 -0600

    Fix open shortcut menu from search results

--- src/screens/Watch/Components/Search/Results/OpenMenuButton.jsx
+++ src/screens/Watch/Components/Search/Results/OpenMenuButton.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { connect } from 'dva';
+    
+const OpenMenuButton = (props)=> {
+  const { show, menu, name, dispatch } = props;
+  const openMenu = () => dispatch({ type: 'watch/menu_open', payload: { type: menu } });
 
-const menuControl = {};
-export default function OpenMenuButton({ show, menu, name }) {
   return show ? (
     <div className="w-100 d-flex px-3">
       <button
         className="plain-btn watch-search-btn page-btn"
         aria-label={`See all ${name}`}
         aria-haspopup="true"
-        onClick={() => menuControl.open(menu)}
+        onClick={() => openMenu(menu)}
       >
         <span className="py-2 px-4 my-1 fsize-1-3" tabIndex="-1">
           See all {name}
@@ -17,3 +20,7 @@ export default function OpenMenuButton({ show, menu, name }) {
     </div>
   ) : null;
 }
+
+export default connect(() => ({
+
+}))(OpenMenuButton);
