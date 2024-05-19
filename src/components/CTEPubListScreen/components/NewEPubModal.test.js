99d65ec44a8f45edf931e35bb9a7b4459e2cbbeb
Harsh Deep <hrshdeep51@gmail.com>
2024-05-19 11:56:11 -0500

    make eslint happy

--- src/components/CTEPubListScreen/components/NewEPubModal.test.js
+++ src/components/CTEPubListScreen/components/NewEPubModal.test.js
@@ -1,12 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import NewEPubModal from './NewEPubModal';
 import { LanguageConstants } from 'components/CTPlayer';
-
-const noop = () => ({});
+import NewEPubModal from './NewEPubModal';
 
 describe('New EPubModal', () => {
+    const noop = () => ({});
+    const baseProps = {
+        open: true,
+        onClose: noop,
+        onCreate: noop,
+        defaultTitle: ""
+    }
     test('it renders with no languages', () => {
-        render(<NewEPubModal open={true} onClose={noop} onCreate={noop} defaultTitle={""}/>);
+        render(<NewEPubModal {...baseProps} />);
 
         expect(screen.getByText("CREATE NEW I-Note")).toBeVisible();
         expect(screen.getByText("I-Note Title")).toBeVisible();
@@ -22,6 +27,6 @@ describe('New EPubModal', () => {
             LanguageConstants.Spanish,
             LanguageConstants.French
         ]
-        render(<NewEPubModal open={false} onClose={noop} onCreate={noop} defaultTitle={""}/>);
+        render(<NewEPubModal languages={languages} {...baseProps} />);
     });
 });
