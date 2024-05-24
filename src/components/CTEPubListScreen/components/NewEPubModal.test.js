28ac53e7bcdf224a7116ec84b1d6401738624f38
Harsh Deep <hrshdeep51@gmail.com>
2024-05-24 14:40:23 -0500

    Add jest + react testing library for unit testing (#802)
    * add testing libraries
    
    * add test bypass for axios
    
    * first test case
    
    * fix jest dom import
    
    * cleanup babel config
    
    * remove babel config
    
    * add window.env setup
    
    * update linter rules
    
    * make eslint happy
    
    * add mock test calling
    
    * wrap up test cases
    
    * remove babel present-env
    
    * change no op function
    
    * add babel preset-env back

---
+++ src/components/CTEPubListScreen/components/NewEPubModal.test.js
@@ -0,0 +1,50 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { LanguageConstants } from 'components/CTPlayer';
+import NewEPubModal from './NewEPubModal';
+
+const mockSelect = jest.fn();
+jest.mock("../../../layout/CTForm/Select/index.js", () => (props) => {
+    mockSelect(props);
+    return <div />;
+}) 
+
+describe('New EPubModal', () => {
+    const baseProps = {
+        open: true,
+        onClose: jest.fn(),
+        onCreate: jest.fn(),
+        defaultTitle: ""
+    }
+    test('it renders with no languages', () => {
+        render(<NewEPubModal {...baseProps} />);
+
+        expect(screen.getByText("CREATE NEW I-Note")).toBeVisible();
+        expect(screen.getByText("I-Note Title")).toBeVisible();
+        
+        expect(mockSelect).toHaveBeenCalledWith(
+            expect.objectContaining({
+                options: []
+            })
+        );
+    });
+
+    test('it renders given list of languages and uniquely', () => {
+        const languages = [
+            LanguageConstants.English,
+            LanguageConstants.English,
+            LanguageConstants.French,
+            LanguageConstants.Spanish,
+            LanguageConstants.French
+        ]
+        render(<NewEPubModal languages={languages} {...baseProps} />);
+
+        expect(mockSelect).toHaveBeenCalledWith(
+            expect.objectContaining({
+                options:  [
+                    {"text": "English", "value": LanguageConstants.English}, 
+                    {"text": "French", "value": LanguageConstants.French}, 
+                    {"text": "Spanish", "value": LanguageConstants.Spanish}]
+            })
+        );
+    });
+});
