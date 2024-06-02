d8ba902428a9448f3fba9021108e842f557345d2
Harsh Deep <hrshdeep51@gmail.com>
2024-06-01 23:26:30 -0500

    remove empty line

--- src/screens/Admin/Instructors/InstructorList.jsx
+++ src/screens/Admin/Instructors/InstructorList.jsx
@@ -35,7 +35,6 @@ export default function InstructorList({ instructors, loading, currUni, onInacti
     }
 
     return `${instructor.firstName || ''} ${instructor.lastName || ''}`.trim();;
-    
   }
 
   return (
