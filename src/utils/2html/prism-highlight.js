235f152322c41edfd7ff3176b22f87c08ca1f8de
Lawrence Angrave <angrave@illinois.edu>
2026-02-09 21:11:50 +0000

    Lint

--- src/utils/2html/prism-highlight.js
+++ src/utils/2html/prism-highlight.js
@@ -2,7 +2,7 @@
 const registerHighlightLanguages = () => {
 //  require('prismjs/components/prism-jsx.min');
 //  require('prismjs/components/prism-python.min');
-const Prism = require('prismjs');
+require('prismjs');
 const loadLanguages = require('prismjs/components/');
 loadLanguages(['python', 'jsx', 'js']);
 };
