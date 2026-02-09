d1d16b15705d1957bee2e094e30750c2adeb5102
Lawrence Angrave <angrave@illinois.edu>
2026-02-09 21:06:35 +0000

    Upgrade to node 24

--- src/utils/2html/prism-highlight.js
+++ src/utils/2html/prism-highlight.js
@@ -1,6 +1,10 @@
+
 const registerHighlightLanguages = () => {
-  require('prismjs/components/prism-jsx.min');
-  require('prismjs/components/prism-python.min');
+//  require('prismjs/components/prism-jsx.min');
+//  require('prismjs/components/prism-python.min');
+const Prism = require('prismjs');
+const loadLanguages = require('prismjs/components/');
+loadLanguages(['python', 'jsx', 'js']);
 };
 
 export default registerHighlightLanguages;
